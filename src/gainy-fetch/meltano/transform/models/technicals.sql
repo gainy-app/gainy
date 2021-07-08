f1117517986297b5d4e3b5880276df855b60f393
avysotsky <artem@notably.ai>
2021-07-08 12:59:03 +0300

    valuation and technicals

--- src/gainy-fetch/meltano/transform/models/technicals.sql
+++ src/gainy-fetch/meltano/transform/models/technicals.sql
@@ -1,6 +1,25 @@
-{%- set j = expand_json_column('technicals', 'fundamentals') -%}
+{{
+  config(
+    materialized = "table",
+    dist = "symbol",
+    post_hook=[
+      index(this, 'symbol', true),
+      fk(this, 'symbol', 'tickers', 'symbol')
+    ]
+  )
+}}
 
-select
-code as symbol,
- {{ j }}
-from fundamentals
+select code                                            as symbol,
+       (technicals ->> 'Beta')::float                  as beta,
+       (technicals ->> '50DayMA')::float               as day_50_ma,
+       (technicals ->> '200DayMA')::float              as day_200_ma,
+       (technicals ->> '52WeekLow')::float             as week_52_Low,
+       (technicals ->> '52WeekHigh')::float            as week_52_High,
+       (technicals ->> 'ShortRatio')::float            as short_ratio,
+       (technicals ->> 'SharesShort')::float           as shares_short,
+       (technicals ->> 'ShortPercent')::float          as short_percent,
+       (technicals ->> 'SharesShortPriorMonth')::float as shares_short_prior_month
+
+from fundamentals f inner join {{  ref('tickers') }} as t on f.code = t.symbol
+
+
--- src/gainy-fetch/meltano/transform/models/valuation.sql
+++ src/gainy-fetch/meltano/transform/models/valuation.sql
@@ -1,6 +1,22 @@
-{%- set j = read_json_column('valuation', 'fundamentals') -%}
+{{
+  config(
+    materialized = "table",
+    dist = "symbol",
+    post_hook=[
+      index(this, 'symbol', true),
+      fk(this, 'symbol', 'tickers', 'symbol')
+    ]
+  )
+}}
 
-select
-    code as symbol,
- {{ j }}
-from fundamentals
+select code                                   as symbol,
+       valuation ->> 'ForwardPE'              as forward_pe,
+       valuation ->> 'TrailingPE'             as trailing_pe,
+       valuation ->> 'PriceBookMRQ'           as price_book_mrq,
+       valuation ->> 'PriceSalesTTM'          as price_sales_ttm,
+       valuation ->> 'EnterpriseValue'        as enterprise_value,
+       valuation ->> 'EnterpriseValueEbitda'  as enterprise_value_ebidta,
+       valuation ->> 'EnterpriseValueRevenue' as enterprise_value_revenue
+from fundamentals f inner join {{  ref('tickers') }} as t on f.code = t.symbol
+
+
