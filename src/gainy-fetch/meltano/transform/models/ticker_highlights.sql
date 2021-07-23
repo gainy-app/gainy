0a40cd916e84541ce250508f31a2a4c44f369ea4
avysotsky <artem@notably.ai>
2021-07-23 12:24:38 +0300

    add second highlight

---
+++ src/gainy-fetch/meltano/transform/models/ticker_highlights.sql
@@ -0,0 +1,57 @@
+{{
+  config(
+    materialized = "table",
+    sort = "created_at",
+    dist = "symbol",
+    post_hook=[
+      index(this, 'symbol', true)
+    ]
+  )
+}}
+    with upcoming_reports as (
+        select symbol,
+               min(report_date) as upcoming_report
+        from earnings_history
+        where eps_actual is null
+        group by symbol
+    ),
+         recent_reports as (
+             select symbol, max(report_date) as recent_report
+             from earnings_history
+             where eps_difference is not null
+             group by symbol),
+         distinct_reports as (
+             select report_date,
+                    symbol,
+                    eps_difference,
+                    max(date)                                                         as date,
+                    ROW_NUMBER() over (partition by symbol order by report_date desc) as r
+             from earnings_history
+             where eps_difference is not null
+               and report_date < now()
+             group by report_date, symbol, eps_difference
+             order by date desc
+         )
+    select symbol,
+           symbol || ' went public on ' || ipo_date as highlight,
+           now()                   as created_at
+    from tickers
+    where ipo_date > now() - interval '3 months'
+    union
+    select distinct_reports.symbol,
+           distinct_reports.symbol || ' reports earnings on ' || max(upcoming_report) || '. Beaten analysts expectations ' ||
+           count(1) || ' of 4 last quarters.' as highlight,
+           now()                   as created_at
+    from distinct_reports
+             left join recent_reports on recent_reports.symbol = distinct_reports.symbol
+             left join upcoming_reports on distinct_reports.symbol = upcoming_reports.symbol
+    where eps_difference > 0
+      and r <= 4
+      and upcoming_report <= now() + interval '2 weeks'
+      and upcoming_report > now()
+    group by distinct_reports.symbol
+    having count(1) >= 3
+
+
+
+
