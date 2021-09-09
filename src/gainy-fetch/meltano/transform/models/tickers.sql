0d767ac9e92a6af6fed2378f778d2b65d49966da
Mikhail Astashkevich <asmisha@tut.by>
2021-09-09 09:29:42 +0300

    implement country name guessing

--- src/gainy-fetch/meltano/transform/models/tickers.sql
+++ src/gainy-fetch/meltano/transform/models/tickers.sql
@@ -9,23 +9,32 @@
   )
 }}
 
-select (general->>'Code')::character varying           as symbol,
-       (general->>'Type')::character varying           as type,
-       (general->>'Name')::character varying           as name,
-       (general->'Description')::character varying     as description,
-       (general->>'Phone')::character varying          as phone,
-       (general->>'LogoURL')::character varying        as logo_url,
-       (general->>'WebURL')::character varying         as web_url,
-       (general->>'IPODate')::date                     as ipo_date,
-       (general->>'Sector')::character varying         as sector,
-       (general->>'Industry')::character varying       as industry,
-       (general->>'GicSector')::character varying      as gic_sector,
-       (general->>'GicGroup')::character varying       as gic_group,
-       (general->>'GicIndustry')::character varying    as gic_industry,
-       (general->>'GicSubIndustry')::character varying as gic_sub_industry,
-       (general->>'CountryName')::character varying    as country_name,
-       (general->>'UpdatedAt')::timestamp              as updated_at
+select (general ->> 'Code')::character varying           as symbol,
+       (general ->> 'Type')::character varying           as type,
+       (general ->> 'Name')::character varying           as name,
+       (general -> 'Description')::character varying     as description,
+       (general ->> 'Phone')::character varying          as phone,
+       (general ->> 'LogoURL')::character varying        as logo_url,
+       (general ->> 'WebURL')::character varying         as web_url,
+       (general ->> 'IPODate')::date                     as ipo_date,
+       (general ->> 'Sector')::character varying         as sector,
+       (general ->> 'Industry')::character varying       as industry,
+       (general ->> 'GicSector')::character varying      as gic_sector,
+       (general ->> 'GicGroup')::character varying       as gic_group,
+       (general ->> 'GicIndustry')::character varying    as gic_industry,
+       (general ->> 'GicSubIndustry')::character varying as gic_sub_industry,
+       -- TODO while this is good enough for Europe, China and LatAm - other countries should be rechecked.
+       --  For instance "South Korea" is not the official name, as well as "United States"
+       coalesce(
+                       general -> 'AddressData' ->> 'Country', -- it's good but there are 65 tickets without it set
+                       case
+                           when TRIM(both reverse(split_part(reverse(general ->> 'Address'), ',', 1))) ~ '[0-9]'
+                               then TRIM(both reverse(split_part(reverse(general ->> 'Address'), ',', 2)))
+                           else TRIM(both reverse(split_part(reverse(general ->> 'Address'), ',', 1)))
+                           end, -- fallback if previous one is null
+                       general ->> 'CountryName' -- it's USA for all companies in EOD
+           )                                             as country_name,
+       (general ->> 'UpdatedAt')::timestamp              as updated_at
 from fundamentals
-where
-    (general->>'IsDelisted')::boolean = false and
-    (general->>'Sector')::character varying is not null
+where (general ->> 'IsDelisted')::boolean = false
+  and (general ->> 'Sector') is not null
