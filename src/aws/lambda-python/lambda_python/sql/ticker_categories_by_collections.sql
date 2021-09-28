f66610ae861d4a4776042e703cbd3d3934b40545
Vasilii Khomutov <vasilii@gainy.app>
2021-09-28 18:36:19 +0300

    #110 Implement ticker match score
    **Changes:**
    * Adds more changes from #110

---
+++ src/aws/lambda-python/lambda_python/sql/ticker_categories_by_collections.sql
@@ -0,0 +1,12 @@
+with ticker_category_vectors as (
+    select symbol, json_object_agg(category_id, 1.0) as ticker_category_vector
+    from public.ticker_categories
+    group by symbol
+)
+select t.symbol, tc.collection_id, tcv.ticker_category_vector
+from public.tickers t
+         left join ticker_category_vectors tcv
+                   on t.symbol = tcv.symbol
+         left join public.ticker_collections as tc
+                   on t.symbol = tc.symbol
+where tc.collection_id in %(collection_ids)s;
---
+++ src/aws/lambda-python/lambda_python/sql/ticker_industries.sql
@@ -0,0 +1,10 @@
+with ticker_industry_vectors as (
+    select symbol, json_object_agg(industry_id, 1.0) as ticker_industry_vector
+    from public.ticker_industries
+    group by symbol
+)
+select t.symbol, tiv.ticker_industry_vector
+from public.tickers t
+         left join ticker_industry_vectors tiv
+                   on t.symbol = tiv.symbol
+where t.symbol = %(symbol)s;
