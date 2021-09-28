e8d1a5e91be86716bedc430ef89c2e29b3f57e0d
Mikhail Astashkevich <asmisha@tut.by>
2021-09-28 14:48:59 +0300

    test env

--- terraform/aws/vars.tf
+++ terraform/aws/vars.tf
@@ -11,10 +11,10 @@ locals {
   ecs_instance_type                    = var.env == "production" ? "c5.2xlarge" : "t3.medium"
   meltano_eodhistoricaldata_jobs_count = var.env == "production" ? 4 : 1
 
-  hasura_cpu_credits            = var.env == "production" ? 512 : 128
-  meltano_scheduler_cpu_credits = var.env == "production" ? 3072 : 256
+  hasura_cpu_credits                   = var.env == "production" ? 512  : 128
+  meltano_scheduler_cpu_credits        = var.env == "production" ? 3072 : 256
 
-  hasura_memory_credits            = var.env == "production" ? 1024 : 512
-  meltano_ui_memory_credits        = var.env == "production" ? 1024 : 1024
-  meltano_scheduler_memory_credits = var.env == "production" ? 3072 : 1024
+  hasura_memory_credits                = var.env == "production" ? 1024 : 512
+  meltano_ui_memory_credits            = var.env == "production" ? 1024 : 1024
+  meltano_scheduler_memory_credits     = var.env == "production" ? 3072 : 1024
 }
