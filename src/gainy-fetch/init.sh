78589d90086daee1533187c6df4d3bb89f279538
Mikhail Astashkevich <asmisha@tut.by>
2021-09-30 10:51:46 +0300

    revert fundamentals sql to native pg_dump format

--- src/gainy-fetch/init.sh
+++ src/gainy-fetch/init.sh
@@ -7,10 +7,16 @@ echo 'Importing seeds'
 find seed -iname '*.sql' | while read -r i; do
   PGPASSWORD=$PG_PASSWORD psql -h $PG_ADDRESS -p $PG_PORT -U $PG_USERNAME $PG_DATABASE -P pager -f "$i"
 done
+echo "Seeding done"
 meltano invoke dbt test -x &> /dev/null || (echo 'Running cst-to-postgres' && meltano schedule run csv-to-postgres)
 
-if [ -z "$NO_AIRFLOW" ] && ! meltano invoke airflow users list | grep admin > /dev/null; then
-  meltano invoke airflow users create --username admin --password $AIRFLOW_PASSWORD --firstname admin --lastname admin --role Admin --email support@gainy.app
+if [ -z "$NO_AIRFLOW" ]; then
+  if ! meltano invoke airflow users list | grep admin > /dev/null; then
+    echo "Creating admin user"
+    meltano invoke airflow users create --username admin --password $AIRFLOW_PASSWORD --firstname admin --lastname admin --role Admin --email support@gainy.app
+  else
+    echo "Admin user exists"
+  fi
 fi
 
 ( cd scripts && python3 generate_rules_sql.py )
