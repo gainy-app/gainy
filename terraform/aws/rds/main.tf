4d7008cfcb8d5ade9790c6819abbad901946d132
Mikhail Astashkevich <asmisha@tut.by>
2021-11-23 18:19:03 +0300

    add rds storage

--- terraform/aws/rds/main.tf
+++ terraform/aws/rds/main.tf
@@ -27,7 +27,7 @@ resource "aws_db_instance" "db_instance" {
   engine_version          = "12"
   instance_class          = var.env == "production" ? "db.m6g.large" : "db.t3.small"
   allocated_storage       = var.env == "production" ? 100 : 30
-  max_allocated_storage   = var.env == "production" ? 200 : 50
+  max_allocated_storage   = var.env == "production" ? 200 : 60
   backup_retention_period = var.env == "production" ? 7 : 0
   storage_type            = var.env == "production" ? "io1" : "gp2"
   iops                    = var.env == "production" ? 1000 : null
