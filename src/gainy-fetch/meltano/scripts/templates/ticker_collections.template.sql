ae927f3ca45b97016aa13445c842b27b37820f97
Medvenator <61165874+Medvenator@users.noreply.github.com>
2021-11-30 01:08:24 +0300

    Update ticker_collections.template.sql
    1) added CTEs:
    price_1month
    price_1year
    
    2) in "ct" CTE:
    renamed field "chrt" to "chrt_1d" (will change relevant collection's sql rules, sure)
    added field "chrt_1m"
    added field "chrt_1y"
    left-joined "price_1month" and "price_1year"

--- src/gainy-fetch/meltano/scripts/templates/ticker_collections.template.sql
+++ src/gainy-fetch/meltano/scripts/templates/ticker_collections.template.sql
@@ -34,6 +34,24 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                and date > NOW() - interval '1 week'
              order by hp.code, hp.date DESC --we get latest by date not-null prices (if any)
          ),
+     price_1month AS
+        (
+             select distinct on (hp.code) hp.*
+             from historical_prices hp
+             where close is not null --need only close
+               and date < NOW() - interval '0.5 month' --buffer 0.5 month to catch some data up to 1 month
+               and date >= NOW() - interval '1 month'
+             order by hp.code, hp.date ASC
+        ),
+     price_1year AS
+        (
+             select distinct on (hp.code) hp.*
+             from historical_prices hp
+             where close is not null --need only close
+               and date < NOW() - interval '1 month' --if company new we just try to get most earliest date
+               and date >= NOW() - interval '1 year' --up to 1 year but farther than 1 month to not overlap selection with previous one
+             order by hp.code, hp.date ASC
+        ),
      ct as
          (
              select t.symbol              as ticker_code,
@@ -43,7 +61,15 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                     CASE
                         WHEN latest_price.close > 0 AND latest_price.open > 0
                             THEN (latest_price.close - latest_price.open) / latest_price.open --else NULL
-                        END               as chrt,
+                        END               as chrt_1d,
+                    CASE
+                        WHEN latest_price.close > 0 AND price_1month.close > 0
+                            THEN (latest_price.close - price_1month.close) / price_1month.close --else NULL
+                        END               as chrt_1m,
+                    CASE
+                        WHEN latest_price.close > 0 AND price_1year.close > 0
+                            THEN (latest_price.close - price_1year.close) / price_1year.close --else NULL
+                        END               as chrt_1y,
                     t.type                as ttype,
                     t.gic_sector          as gics_sector,
                     gainy_industries.name as g_industry,
@@ -58,7 +84,9 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                       LEFT JOIN gainy_industries on ticker_industries.industry_id = gainy_industries.id
                       LEFT JOIN ticker_interests on t.symbol = ticker_interests.symbol
                       LEFT JOIN interests on ticker_interests.interest_id = interests.id
-                      LEFT JOIN latest_price ON latest_price.code = t.symbol
+                      LEFT JOIN latest_price ON latest_price.code = t.symbol           
+                      LEFT JOIN price_1month ON price_1month.code = t.symbol
+                      LEFT JOIN price_1year ON price_1year.code = t.symbol           
                       LEFT JOIN ticker_categories tc
                                 on t.symbol = tc.symbol --here we have N:N relationship, so for interests we must use distinct in the end (we will get duplicates otherwise)
                       LEFT JOIN categories c on tc.category_id = c.id
