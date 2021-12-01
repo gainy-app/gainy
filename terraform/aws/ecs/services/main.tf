c1456f9fc0d0bc86d81f7b47daf218d110be50aa
Vasilii Khomutov <vasilii@gainy.app>
2021-12-01 21:06:03 +0300

    * Datadog config for airflow once again

--- terraform/aws/ecs/services/main.tf
+++ terraform/aws/ecs/services/main.tf
@@ -142,6 +142,9 @@ resource "aws_ecs_task_definition" "default" {
       algolia_collections_index            = var.algolia_collections_index
       algolia_app_id                       = var.algolia_app_id
       algolia_indexing_key                 = var.algolia_indexing_key
+
+      datadog_api_key                      = var.datadog_api_key
+      datadog_app_key                      = var.datadog_app_key
     }
   )
 }
--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -122,13 +122,16 @@ module "ecs-service" {
   scheduler_memory_credits     = local.meltano_scheduler_memory_credits
   ui_memory_credits            = local.meltano_ui_memory_credits
 
-  base_image_registry_address = var.base_image_registry_address
-  base_image_version          = var.base_image_version
+  base_image_registry_address  = var.base_image_registry_address
+  base_image_version           = var.base_image_version
 
-  algolia_tickers_index     = var.algolia_tickers_index
-  algolia_collections_index = var.algolia_collections_index
-  algolia_app_id            = var.algolia_app_id
-  algolia_indexing_key      = var.algolia_indexing_key
+  algolia_tickers_index        = var.algolia_tickers_index
+  algolia_collections_index    = var.algolia_collections_index
+  algolia_app_id               = var.algolia_app_id
+  algolia_indexing_key         = var.algolia_indexing_key
+
+  datadog_api_key              = var.datadog_api_key
+  datadog_app_key              = var.datadog_app_key
 }
 
 output "bridge_instance_url" {
