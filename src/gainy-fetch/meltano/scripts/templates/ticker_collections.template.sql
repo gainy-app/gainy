805bb4aa0e6f2350d5375a1f4f7e85dfc901b46e
Medvenator <61165874+Medvenator@users.noreply.github.com>
2021-12-09 17:01:05 +0300

    Update ticker_collections.template.sql
    1. deleted "technicals as (select * from {{ ref('technicals') }})" and ct field "technicals.short_percent" -> switched to "ticker_metrics.short_percent" -> ! need to add that field to ticker_metrics model !
    2. deleted custom cte price_1y -> switched to ticker_metrics.price_change_1y
    3. deleted custom cte price_1m -> switched to ticker_metrics.price_change_1m

--- src/gainy-fetch/meltano/scripts/templates/ticker_collections.template.sql
+++ src/gainy-fetch/meltano/scripts/templates/ticker_collections.template.sql
@@ -25,7 +25,6 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
      categories as (select * from {{ ref('categories') }}),
      collections as (select id::int, name from {{ source('gainy', 'gainy_collections') }} where personalized = '0'),
      countries as (select * from {{ source('gainy', 'gainy_countries') }}),
-     technicals as (select * from {{ ref('technicals') }}),
      ticker_metrics as (select * from {{ ref('ticker_metrics') }}),
      latest_price AS
          (
@@ -36,24 +35,6 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                and date > NOW() - interval '1 week'
              order by hp.code, hp.date DESC --we get latest by date not-null prices (if any)
          ),
-     price_1month AS
-        (
-             select distinct on (hp.code) hp.*
-             from historical_prices hp
-             where close is not null --need only close
-               and date < NOW() - interval '0.5 month' --buffer 0.5 month to catch some data up to 1 month
-               and date >= NOW() - interval '1 month'
-             order by hp.code, hp.date ASC
-        ),
-     price_1year AS
-        (
-             select distinct on (hp.code) hp.*
-             from historical_prices hp
-             where close is not null --need only close
-               and date < NOW() - interval '1 month' --if company new we just try to get most earliest date
-               and date >= NOW() - interval '1 year' --up to 1 year but farther than 1 month to not overlap selection with previous one
-             order by hp.code, hp.date ASC
-        ),
      t_ranks AS
         (
              select tm.symbol,
@@ -73,14 +54,8 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                         WHEN latest_price.close > 0 AND latest_price.open > 0
                             THEN (latest_price.close - latest_price.open) / latest_price.open --else NULL
                         END                                   as chrt_1d,
-                    CASE
-                        WHEN latest_price.close > 0 AND price_1month.close > 0
-                            THEN (latest_price.close - price_1month.close) / price_1month.close --else NULL
-                        END                                   as chrt_1m,
-                    CASE
-                        WHEN latest_price.close > 0 AND price_1year.close > 0
-                            THEN (latest_price.close - price_1year.close) / price_1year.close --else NULL
-                        END                                   as chrt_1y,
+                    ticker_metrics.price_change_1m            as chrt_1m,
+                    ticker_metrics.price_change_1y            as chrt_1y,
                     t.type                                    as ttype,
                     t.gic_sector                              as gics_sector,
                     gainy_industries.name                     as g_industry,
@@ -90,7 +65,7 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                         WHEN countries.region = 'Europe' THEN 'europe'
                         WHEN countries."sub-region" LIKE '%Latin America%' THEN 'latam'
                         END                                   as country_group,
-                    technicals.short_percent,
+                    ticker_metrics.short_percent,
                     t_ranks.vol_10d_topintype_order,
                     t_ranks.vol_10d_topintype_percent
              from tickers t
@@ -99,15 +74,12 @@ with historical_prices as (select * from {{ ref('historical_prices') }}),
                       LEFT JOIN ticker_interests on t.symbol = ticker_interests.symbol --here we have N:N relationship, so we must use distinct in the end (we will get duplicates otherwise)
                       LEFT JOIN interests on ticker_interests.interest_id = interests.id
                       LEFT JOIN latest_price ON latest_price.code = t.symbol           
-                      LEFT JOIN price_1month ON price_1month.code = t.symbol
-                      LEFT JOIN price_1year ON price_1year.code = t.symbol           
                       LEFT JOIN ticker_categories tc
                                 on t.symbol = tc.symbol --here we have N:N relationship, so we must use distinct in the end (we will get duplicates otherwise)
                       LEFT JOIN categories c on tc.category_id = c.id
                       LEFT JOIN countries
                                 on countries.name = t.country_name OR countries."alpha-2" = t.country_name OR
                                    countries."alpha-3" = t.country_name
-                      LEFT JOIN technicals on t.symbol = technicals.symbol
                       LEFT JOIN t_ranks on t_ranks.symbol = t.symbol
          ),
      tmp_ticker_collections as
