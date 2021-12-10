e6fa78fb63d68081167b4f28716f7bce0d4401e5
Mikhail Astashkevich <asmisha@tut.by>
2021-12-10 18:42:07 +0300

    fetch ticker options

---
+++ src/gainy-fetch/meltano/transform/models/ticker_options.sql
@@ -0,0 +1,63 @@
+{{
+  config(
+    materialized = "incremental",
+    unique_key = "contract_name",
+    incremental_strategy = 'insert_overwrite',
+    post_hook=[
+      index(this, 'contract_name', true),
+      index(this, 'symbol', false),
+    ]
+  )
+}}
+
+
+with
+{% if is_incremental() %}
+     max_updated_at as (select symbol, max(updated_at) as max_date from ticker_options group by symbol),
+{% endif %}
+expanded as
+    (
+        select code                                                  as symbol,
+               json_array_elements((json_each(options::json)).value) as value
+
+        from {{ source('eod', 'eod_options') }} f
+                 inner join tickers as t
+                            on f.code = t.symbol
+    )
+select expanded.symbol,
+       (value ->> 'ask')::float                as ask,
+       (value ->> 'bid')::float                as bid,
+       (value ->> 'change')::float             as change,
+       (value ->> 'changePercent')::float      as change_percent,
+       (value ->> 'contractName')::varchar     as contract_name,
+       (value ->> 'contractPeriod')::varchar   as contract_period,
+       (value ->> 'contractSize')::varchar     as contract_size,
+       (value ->> 'currency')::varchar         as currency,
+       (value ->> 'daysBeforeExpiration')::int as days_before_expiration,
+       (value ->> 'delta')::float              as delta,
+       (value ->> 'expirationDate')::date      as expiration_date,
+       (value ->> 'gamma')::float              as gamma,
+       (value ->> 'impliedVolatility')::float  as implied_volatility,
+       lower(value ->> 'inTheMoney') = 'true'  as in_the_money,
+       (value ->> 'intrinsicValue')::float     as intrinsic_value,
+       (value ->> 'lastPrice')::float          as last_price,
+       case
+           when (value ->> 'lastTradeDateTime') != '0000-00-00 00:00:00'
+               then (value ->> 'lastTradeDateTime')::timestamp
+           end                                 as last_trade_datetime,
+       (value ->> 'openInterest')::int         as open_interest,
+       (value ->> 'rho')::float                as rho,
+       (value ->> 'strike')::float             as strike,
+       (value ->> 'theoretical')::float        as theoretical,
+       (value ->> 'theta')::float              as theta,
+       (value ->> 'timeValue')::float          as time_value,
+       (value ->> 'type')::varchar             as type,
+       (value ->> 'updatedAt')::timestamp      as updated_at,
+       (value ->> 'vega')::float               as vega,
+       (value ->> 'volume')::int               as volume
+from expanded
+{% if is_incremental() %}
+         left join max_updated_at on expanded.symbol = max_updated_at.symbol
+where (value ->> 'updatedAt')::timestamp >= max_updated_at.max_date
+   or max_updated_at.max_date is null
+{% endif %}
