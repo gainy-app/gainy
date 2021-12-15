f8b16b212e9b050e0edce3ff3739027624301e85
Mikhail Astashkevich <asmisha@tut.by>
2021-12-15 17:35:10 +0300

    fix holding name

--- src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
+++ src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
@@ -9,10 +9,12 @@ select profile_holdings.id as holding_id,
        profile_holdings.profile_id,
        profile_holdings.account_id,
        profile_holdings.quantity,
-       portfolio_securities_normalized.name,
+       tickers.name,
        portfolio_securities_normalized.ticker_symbol,
        portfolio_securities_normalized.type
 from {{ source('app', 'profile_holdings') }}
          join {{ ref('portfolio_securities_normalized') }}
               on portfolio_securities_normalized.id = profile_holdings.security_id
+         join {{ ref('tickers') }}
+              on tickers.symbol = portfolio_securities_normalized.ticker_symbol
 where portfolio_securities_normalized.type in ('mutual fund', 'equity', 'etf', 'derivative', 'cash')
