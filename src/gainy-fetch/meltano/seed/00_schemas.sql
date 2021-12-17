605a2e173619f92fdbd1e85628db13cf8d32fd37
Vasilii Khomutov <vasilii@gainy.app>
2021-12-17 19:29:22 +0300

    * Remove `mlflow` schema creation

--- src/gainy-fetch/meltano/seed/00_schemas.sql
+++ src/gainy-fetch/meltano/seed/00_schemas.sql
@@ -1,6 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS meltano;
 CREATE SCHEMA IF NOT EXISTS airflow;
-CREATE SCHEMA IF NOT EXISTS mlflow;
 CREATE SCHEMA IF NOT EXISTS raw_data;
 
 create table if not exists raw_data.eod_intraday_prices
