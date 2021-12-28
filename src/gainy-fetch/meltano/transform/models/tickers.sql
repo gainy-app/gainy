6b59e4d6f8d6869392e607832dbefca90dffc7ab
Mikhail Astashkevich <asmisha@tut.by>
2021-12-28 08:41:00 +0300

    Disable tickers without prices newer than week ago

--- src/gainy-fetch/meltano/transform/models/tickers.sql
+++ src/gainy-fetch/meltano/transform/models/tickers.sql
@@ -11,14 +11,24 @@
 }}
 
 with volumes as (
-	select code as symbol, avg(volume) as avg_volume
-	from {{ source('eod', 'eod_historical_prices') }}
-	where "date"::date >= NOW() - interval '30 days'
-	group by code
-)
+         select code as symbol, avg(volume) as avg_volume
+         from {{ source('eod', 'eod_historical_prices') }}
+         where "date"::date >= NOW() - interval '30 days'
+         group by code
+     ),
+     latest_price as (
+         select max(date) as date
+         from {{ source('eod', 'eod_historical_prices') }}
+         group by code
+     )
 select t.*
 from {{ ref('base_tickers') }} t
-    join volumes v
-        on t.symbol = v.symbol
-where (v.avg_volume is not null and v.avg_volume >= {{ min_daily_volume }}) and (t.description is not null and length(t.description) >= 5)
+    join volumes v on t.symbol = v.symbol
+    join latest_price on t.symbol = latest_price.symbol
+where v.avg_volume is not null
+  and v.avg_volume >= {{ min_daily_volume }}
+  and t.description is not null
+  and length (t.description) >= 5
+  and latest_price.date is not null
+  and latest_price.date >= now() - interval '7 days'
 
