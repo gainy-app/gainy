597b58019a47d5f6c36416944ab95e571ddee4ed
Mikhail Astashkevich <asmisha@tut.by>
2021-12-28 08:14:16 +0300

    Fix missing collections during deployment

--- src/gainy-fetch/meltano/transform/models/collections.sql
+++ src/gainy-fetch/meltano/transform/models/collections.sql
@@ -9,15 +9,26 @@
 }}
 
 
-with collections as (
-    select id::int, name, description, enabled, personalized, image_url
-    from {{ source ('gainy', 'gainy_collections') }}
-)
+with
+{% if is_incremental() %}
+     old_collections as (select * from collections),
+{% endif %}
+     collections as (
+         select id::int, name, description, enabled, personalized, image_url
+         from raw_data.gainy_collections
+     )
 select c.id,
        c.name,
        c.description,
        c.enabled,
        c.personalized,
        c.image_url,
+{% if is_incremental() %}
+       coalesce(old_collections.size, 0::integer) as size
+{% else %}
        0::integer as size
+{% endif %}
 from collections c
+{% if is_incremental() %}
+         left join old_collections on old_collections.id = c.id
+{% endif %}
