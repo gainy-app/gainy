2cd498185dd1fc707e864537fbfbdd2d5bb29794
Vasilii Khomutov <vasilii@gainy.app>
2022-01-17 18:47:30 +0300

    Refactoring - simplify and purify recommendation logic

--- src/aws/lambda-python/lambda_python/trigger/set_recommendations.py
+++ src/aws/lambda-python/lambda_python/trigger/set_recommendations.py
@@ -6,32 +6,6 @@ from recommendation.match_score import MatchScore, profile_ticker_similarity
 from recommendation import TOP_20_FOR_YOU_COLLECTION_ID
 
 
-def get_and_sort_by_match_score(db_conn,
-                                profile_id: int,
-                                top_k: int = None) -> List[Tuple[str, MatchScore]]:
-    repository = RecommendationRepository(db_conn)
-
-    profile_category_v = repository.read_profile_category_vector(profile_id)
-    profile_interest_vs = repository.read_profile_interest_vectors(profile_id)
-
-    risk_mapping = repository.read_categories_risks()
-
-    ticker_vs_list = repository.read_all_ticker_category_and_industry_vectors()
-
-    match_score_list = []
-    for ticker_vs in ticker_vs_list:
-        match_score = profile_ticker_similarity(profile_category_v,
-                                                ticker_vs[1], risk_mapping,
-                                                profile_interest_vs,
-                                                ticker_vs[0])
-        match_score_list.append((ticker_vs[0].name, match_score))
-
-    # Uses minus `match_score` to correctly sort the list by both score and symbol
-    match_score_list.sort(key=lambda m: (-m[1].match_score(), m[0]))
-
-    return match_score_list[:top_k] if top_k else match_score_list
-
-
 class SetRecommendations(HasuraTrigger):
 
     def __init__(self):
@@ -44,13 +18,34 @@ class SetRecommendations(HasuraTrigger):
     def apply(self, db_conn, op, data):
         profile_id = self.get_profile_id(op, data)
 
-        tickers_with_match_score = get_and_sort_by_match_score(db_conn, profile_id)
-
         repository = RecommendationRepository(db_conn)
+
+        tickers_with_match_score = self.get_and_sort_by_match_score(repository, profile_id)
         repository.update_match_score(profile_id, tickers_with_match_score)
 
         top_20_tickers = [ticker[0] for ticker in tickers_with_match_score[:20]]
         repository.update_personalized_collection(profile_id, TOP_20_FOR_YOU_COLLECTION_ID, top_20_tickers)
+
+    def get_and_sort_by_match_score(self, repository, profile_id: int, top_k: int = None) -> List[Tuple[str, MatchScore]]:
+        profile_category_v = repository.read_profile_category_vector(profile_id)
+        profile_interest_vs = repository.read_profile_interest_vectors(profile_id)
+
+        risk_mapping = repository.read_categories_risks()
+
+        ticker_vs_list = repository.read_all_ticker_category_and_industry_vectors()
+
+        match_score_list = []
+        for ticker_vs in ticker_vs_list:
+            match_score = profile_ticker_similarity(profile_category_v,
+                                                    ticker_vs[1], risk_mapping,
+                                                    profile_interest_vs,
+                                                    ticker_vs[0])
+            match_score_list.append((ticker_vs[0].name, match_score))
+
+        # Uses minus `match_score` to correctly sort the list by both score and symbol
+        match_score_list.sort(key=lambda m: (-m[1].match_score(), m[0]))
+
+        return match_score_list[:top_k] if top_k else match_score_list
 
     def get_profile_id(self, op, data):
         payload = self._extract_payload(data)
