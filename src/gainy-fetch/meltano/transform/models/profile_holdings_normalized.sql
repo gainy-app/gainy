06dfce9975717363836338a50d3037f418ab91d0
Mikhail Astashkevich <asmisha@tut.by>
2022-01-18 08:57:14 +0300

    refactor name

--- src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
+++ src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
@@ -4,8 +4,8 @@
   )
 }}
 
-with robinhood_options_quantity_divisible_by_hundred as (
-    select sum(mod(profile_holdings.quantity::int, 100)) as module_sum
+with robinhood_options as (
+    select sum(mod(profile_holdings.quantity::int, 100)) as quantity_module_sum
     from {{ source('app', 'profile_holdings') }}
              join {{ source('app', 'portfolio_securities') }} on portfolio_securities.id = profile_holdings.id
              left join {{ source('app', 'profile_plaid_access_tokens') }} on profile_plaid_access_tokens.id = profile_holdings.plaid_access_token_id
@@ -19,7 +19,7 @@ select profile_holdings.id                        as holding_id,
        profile_holdings.profile_id,
        profile_holdings.account_id,
        profile_holdings.quantity / case
-                                       when robinhood_options_quantity_divisible_by_hundred.module_sum = 0 and
+                                       when robinhood_options.quantity_module_sum = 0 and
                                             portfolio_securities_normalized.type = 'derivative' and
                                             plaid_institutions.ref_id = 'ins_54' then 100
                                        else 1 end as quantity,
@@ -33,5 +33,5 @@ from {{ source('app', 'profile_holdings') }}
               on tickers.symbol = portfolio_securities_normalized.ticker_symbol
          left join {{ source('app', 'profile_plaid_access_tokens') }} on profile_plaid_access_tokens.id = profile_holdings.plaid_access_token_id
          left join {{ source('app', 'plaid_institutions') }} on plaid_institutions.id = profile_plaid_access_tokens.institution_id
-         join robinhood_options_quantity_divisible_by_hundred on true
+         join robinhood_options on true
 where portfolio_securities_normalized.type in ('mutual fund', 'equity', 'etf', 'derivative', 'cash')
