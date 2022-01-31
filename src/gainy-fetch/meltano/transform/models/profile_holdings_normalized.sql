f4181d692ca80dfc6a0768cd5e773f9cdb9e91d3
Mikhail Astashkevich <asmisha@tut.by>
2022-01-31 18:40:23 +0300

    fix securities

--- src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
+++ src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
@@ -24,8 +24,8 @@ select profile_holdings.id                        as holding_id,
                                        when robinhood_options.quantity_module_sum = 0 and
                                             portfolio_securities_normalized.type = 'derivative' and
                                             plaid_institutions.ref_id = 'ins_54' then 100
-                                       else 1 end as quantity,
-       tickers.name,
+                                       else 1 end                   as quantity,
+       coalesce(tickers.name, portfolio_securities_normalized.name) as name,
        portfolio_securities_normalized.ticker_symbol,
        portfolio_securities_normalized.type
 from {{ source('app', 'profile_holdings') }}
