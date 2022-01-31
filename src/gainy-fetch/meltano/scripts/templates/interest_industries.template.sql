446361e7566c13b62a114a6f098f0c9a0e115d54
Vasilii Khomutov <vasilii@gainy.app>
2022-01-31 20:33:00 +0300

    Add an additional `interest_interests` model

---
+++ src/gainy-fetch/meltano/scripts/templates/interest_industries.template.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized = "incremental",
+    unique_key = "id",
+    post_hook=[
+      index(this, 'id', true),
+      'create unique index if not exists {{ get_index_name(this, "interest_id__industry_id") }} (interest_id, industry_id)',
+      'delete from {{this}} where updated_at < (select max(updated_at) from {{this}})',
+    ]
+  )
+}}
+
+/* THE MODEL FILE IS AUTO GENERATED DURING BUILD, YOU SHOULD NOT EDIT THE MODEL, EDIT THE TEMPLATE INSTEAD  */
+
+with
+	ct as (
+		select name as g_industry, id as g_industry_id from {{ ref('gainy_industries') }}
+	),
+    tmp_interest_industries as
+         (
+-- __SELECT__ --
+         )
+SELECT distinct concat(tii.interest_id, '_', tii.industry_id)::varchar as id,
+        tii.interest_id,
+        tii.industry_id,
+        now() as updated_at
+from tmp_interest_industries tii
+         join {{ ref('gainy_industries') }} gi on tii.industry_id = gi.id
+         join {{ ref ('interests') }} i on tii.interest_id = i.id
+
---
+++ src/gainy-fetch/meltano/transform/models/ticker_interests.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized = "incremental",
+    unique_key = "id",
+    post_hook=[
+      index(this, 'id', true),
+      'create unique index if not exists {{ get_index_name(this, "symbol__interest_id") }} (symbol, interest_id)',
+      'delete from {{this}} where updated_at < (select max(updated_at) from {{this}})',
+    ]
+  )
+}}
+
+SELECT distinct concat(ti.symbol, '_', ii.interest_id)::varchar as id,
+                ti.symbol,
+                ii.interest_id,
+                now() as updated_at
+from {{ ref('interest_industries') }} ii
+    join {{ ref ('ticker_industries') }} ti on ii.industry_id = ti.industry_id
