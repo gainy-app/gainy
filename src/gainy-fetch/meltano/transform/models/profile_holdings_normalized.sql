cd5e48e24845d7a3cc58e7221936211070119124
Mikhail Astashkevich <asmisha@tut.by>
2022-02-01 08:10:38 +0300

    fix holdings and retry with historical prices aggregated

--- src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
+++ src/gainy-fetch/meltano/transform/models/profile_holdings_normalized.sql
@@ -25,17 +25,17 @@ select profile_holdings.id                        as holding_id,
                                             portfolio_securities_normalized.type = 'derivative' and
                                             plaid_institutions.ref_id = 'ins_54' then 100
                                        else 1 end                   as quantity,
-       coalesce(tickers.name, portfolio_securities_normalized.name) as name,
+       coalesce(base_tickers.name, portfolio_securities_normalized.name) as name,
        portfolio_securities_normalized.ticker_symbol,
        portfolio_securities_normalized.type
 from {{ source('app', 'profile_holdings') }}
          join {{ ref('portfolio_securities_normalized') }}
               on portfolio_securities_normalized.id = profile_holdings.security_id
-         left join {{ ref('tickers') }}
-              on tickers.symbol = portfolio_securities_normalized.ticker_symbol
+         left join {{ ref('base_tickers') }}
+              on base_tickers.symbol = portfolio_securities_normalized.ticker_symbol
          left join {{ source('app', 'profile_plaid_access_tokens') }} on profile_plaid_access_tokens.id = profile_holdings.plaid_access_token_id
          left join {{ source('app', 'plaid_institutions') }} on plaid_institutions.id = profile_plaid_access_tokens.institution_id
          left join robinhood_options on robinhood_options.profile_id = profile_holdings.profile_id
 where portfolio_securities_normalized.type in ('mutual fund', 'equity', 'etf', 'derivative', 'cash')
   and profile_holdings.quantity > 0
-  and (tickers.symbol is not null or (portfolio_securities_normalized.type = 'cash' and portfolio_securities_normalized.original_ticker_symbol = 'CUR:USD'))
+  and (base_tickers.symbol is not null or (portfolio_securities_normalized.type = 'cash' and portfolio_securities_normalized.original_ticker_symbol = 'CUR:USD'))
