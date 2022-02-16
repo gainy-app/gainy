2b703a49500f8983ec42dd0888cd32065251b90c
Mikhail Astashkevich <asmisha@tut.by>
2022-02-16 14:04:49 +0300

    fix tests

--- src/gainy-fetch/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
+++ src/gainy-fetch/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
@@ -17,7 +17,7 @@ with first_profile_transaction_date as
                     min(date) as datetime
              from {{ source('app', 'profile_portfolio_transactions') }}
              group by profile_id
-{% if is_incremental() and not var('realtime') %}
+{% if is_incremental() %}
          ),
      latest_transaction_chart_row as
          (
@@ -50,7 +50,7 @@ from {{ ref('portfolio_expanded_transactions') }}
                        coalesce(portfolio_expanded_transactions.date, first_profile_transaction_date.datetime) or
                        coalesce(portfolio_expanded_transactions.date,
                                 first_profile_transaction_date.datetime) is null)
-{% if is_incremental() and not var('realtime') %}
+{% if is_incremental() %}
          left join latest_transaction_chart_row
               on latest_transaction_chart_row.transactions_uniq_id = portfolio_expanded_transactions.uniq_id
                   and latest_transaction_chart_row.period = chart.period
@@ -58,7 +58,7 @@ from {{ ref('portfolio_expanded_transactions') }}
 where portfolio_expanded_transactions.type in ('buy', 'sell')
 {% if is_incremental() %}
   {% if var('realtime') %}
-  and chart.period in ('1d', '1w')
+  and (chart.period in ('1d', '1w') or latest_transaction_chart_row.datetime is null)
   {% else %}
   and (chart.period in ('1d', '1w') or latest_transaction_chart_row.datetime is null or chart.datetime >= latest_transaction_chart_row.datetime)
   {% endif %}
