96d43761430e4e614ad76ef958631f931f5e8f16
Mikhail Astashkevich <asmisha@tut.by>
2022-03-02 19:43:11 +0400

    debug

--- deployment/scripts/code_artifactory.sh
+++ deployment/scripts/code_artifactory.sh
@@ -5,4 +5,8 @@ CODEARTIFACT_AUTH_TOKEN="$(aws codeartifact get-authorization-token --domain gai
 CODEARTIFACT_REPO_URL="$(aws codeartifact get-repository-endpoint --domain gainy-app --repository gainy-app --format pypi --query repositoryEndpoint --output text | sed 's/https:\/\///g' )"
 CODEARTIFACT_PIPY_URL=https://aws:${CODEARTIFACT_AUTH_TOKEN}@${CODEARTIFACT_REPO_URL}simple/
 
+aws codeartifact get-repository-endpoint --domain gainy-app --repository gainy-app --format pypi --query repositoryEndpoint --output text | sed 's/https:\/\///g'
+echo $CODEARTIFACT_REPO_URL
+echo $CODEARTIFACT_PIPY_URL
+
 export CODEARTIFACT_PIPY_URL
