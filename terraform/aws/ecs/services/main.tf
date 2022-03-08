3e22578850a44bc34f5f3d27d6167c25ee88c9e4
Mikhail Astashkevich <asmisha@tut.by>
2022-03-08 10:05:26 +0400

    final

--- terraform/aws/ecs/services/main.tf
+++ terraform/aws/ecs/services/main.tf
@@ -105,53 +105,6 @@ resource "docker_registry_image" "websockets" {
  * Create task definition
  */
 
-resource "aws_iam_role" "gainy_task_role" {
-  name               = "ecsInstanceRole-gainy-task-${var.env}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "execute_command" {
-  name        = "gainy_${var.env}"
-  description = "Gainy ECS Execute Command Policy ${var.env}"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssmmessages:CreateControlChannel",
-          "ssmmessages:CreateDataChannel",
-          "ssmmessages:OpenControlChannel",
-          "ssmmessages:OpenDataChannel"
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_gainy_default" {
-  role       = aws_iam_role.gainy_task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_gainy_execute_command" {
-  role       = aws_iam_role.gainy_task_role.name
-  policy_arn = aws_iam_policy.execute_command.arn
-}
 resource "random_password" "airflow" {
   length           = 16
   special          = true
@@ -165,7 +118,6 @@ resource "aws_ecs_task_definition" "default" {
   volume {
     name = "meltano-data"
   }
-  task_role_arn = aws_iam_role.gainy_task_role.arn
 
   container_definitions = templatefile(
     "${path.module}/container-definitions.json",
