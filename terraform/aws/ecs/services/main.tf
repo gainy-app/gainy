81b76173ff62f66d13cb12041ced5a7abbf340da
Mikhail Astashkevich <asmisha@tut.by>
2022-03-10 15:48:09 +0400

    fix and refactor

--- terraform/aws/ecs/services/main.tf
+++ terraform/aws/ecs/services/main.tf
@@ -56,6 +56,98 @@ locals {
       hasura_healthcheck_retries  = 2
     })
   ))
+
+  meltano_default_params = {
+    env                          = var.env
+    pg_host                      = var.pg_host
+    pg_dbname                    = var.pg_dbname
+    pg_password                  = var.pg_password
+    pg_port                      = var.pg_port
+    pg_username                  = var.pg_username
+    pg_meltano_schema            = "meltano"
+    pg_airflow_schema            = "airflow"
+    airflow_password             = random_password.airflow.result
+    airflow_port                 = 5001
+    eodhistoricaldata_jobs_count = var.eodhistoricaldata_jobs_count
+    pg_transform_schema          = local.public_schema_name
+    meltano_image                = docker_registry_image.meltano.name
+    aws_log_group_name           = var.aws_log_group_name
+    aws_log_region               = var.aws_log_region
+  }
+  meltano_airflow_ui_task_description = jsondecode(templatefile(
+    "${path.module}/task_definitions/meltano-airflow-ui.json",
+    merge(local.meltano_default_params, {
+      airflow_ui_memory_credits = var.ui_memory_credits
+    })
+  ))
+  meltano_airflow_scheduler_description = jsondecode(templatefile(
+    "${path.module}/task_definitions/meltano-airflow-scheduler.json",
+    merge(local.meltano_default_params, {
+      eodhistoricaldata_api_token          = var.eodhistoricaldata_api_token
+      eodhistoricaldata_exchanges          = jsonencode(["US", "CC", "INDX"])
+      pg_load_schema                       = "raw_data"
+      dbt_threads                          = var.env == "production" ? 4 : 4
+      pg_production_host                   = var.pg_production_host
+      pg_production_port                   = var.pg_production_port
+      pg_production_internal_sync_username = var.pg_production_internal_sync_username
+      pg_production_internal_sync_password = var.pg_production_internal_sync_password
+      airflow_scheduler_memory_credits     = var.scheduler_memory_credits
+      airflow_scheduler_cpu_credits        = var.scheduler_cpu_credits
+      algolia_tickers_index                = var.algolia_tickers_index
+      algolia_collections_index            = var.algolia_collections_index
+      algolia_app_id                       = var.algolia_app_id
+      algolia_indexing_key                 = var.algolia_indexing_key
+
+      pg_external_access_host     = var.pg_external_access_host
+      pg_external_access_port     = var.pg_external_access_port
+      pg_external_access_username = var.pg_external_access_username
+      pg_external_access_password = var.pg_external_access_password
+      pg_external_access_dbname   = var.pg_external_access_dbname
+      pg_analytics_schema         = var.pg_analytics_schema
+      pg_website_schema           = var.pg_website_schema
+
+      datadog_api_key = var.datadog_api_key
+      datadog_app_key = var.datadog_app_key
+
+      polygon_api_token = var.polygon_api_token
+
+      # mlflow
+      aws_region               = var.aws_region
+      aws_access_key           = var.aws_access_key
+      aws_secret_key           = var.aws_secret_key
+      mlflow_artifact_location = "s3://${var.mlflow_artifact_bucket}"
+      pg_mlflow_schema         = "mlflow"
+    })
+  ))
+
+  websockets_default_params = {
+    env                 = var.env
+    pg_host             = var.pg_host
+    pg_dbname           = var.pg_dbname
+    pg_password         = var.pg_password
+    pg_port             = var.pg_port
+    pg_username         = var.pg_username
+    pg_transform_schema = local.public_schema_name
+    datadog_api_key     = var.datadog_api_key
+    aws_log_group_name  = var.aws_log_group_name
+    aws_log_region      = var.aws_log_region
+    websockets_image    = docker_registry_image.websockets.name
+  }
+  websockets_eod_task_description = jsondecode(templatefile(
+    "${path.module}/task_definitions/websockets-eod.json",
+    merge(local.websockets_default_params, {
+      eodhistoricaldata_api_token   = var.eodhistoricaldata_api_token
+      eod_websockets_memory_credits = var.eod_websockets_memory_credits
+    })
+  ))
+  websockets_polygon_task_description = jsondecode(templatefile(
+    "${path.module}/task_definitions/websockets-polygon.json",
+    merge(local.websockets_default_params, {
+      polygon_websockets_memory_credits = var.polygon_websockets_memory_credits
+      polygon_api_token                 = var.polygon_api_token
+      polygon_realtime_streaming_host   = "delayed.polygon.io" # socket.polygon.io for real-time
+    })
+  ))
 }
 
 resource "random_password" "hasura" {
@@ -64,9 +156,14 @@ resource "random_password" "hasura" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-/*
- * Create an image
- */
+resource "random_password" "airflow" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+###### Create images #####
+
 data "archive_file" "meltano_source" {
   type        = "zip"
   source_dir  = local.meltano_root_dir
@@ -135,125 +232,10 @@ resource "docker_registry_image" "websockets" {
   }
 }
 
-/*
- * Create task definition
- */
+###### Create execution role ######
 
-resource "aws_iam_role" "gainy_task_role" {
-  name               = "gainy-task-${var.env}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_gainy_task_role_default" {
-  role       = aws_iam_role.gainy_task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-resource "random_password" "airflow" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-resource "aws_ecs_task_definition" "default" {
-  family                   = "gainy-${var.env}"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.main_cpu_credits
-  memory                   = var.main_memory_credits
-  tags                     = {}
-  volume {
-    name = "meltano-data"
-  }
-  task_role_arn = aws_iam_role.gainy_task_role.arn
-
-  container_definitions = templatefile(
-    "${path.module}/container-definitions.json",
-    {
-      hasura_enable_console           = var.hasura_enable_console
-      hasura_enable_dev_mode          = var.hasura_enable_dev_mode
-      hasura_admin_secret             = random_password.hasura.result
-      hasura_jwt_secret               = var.hasura_jwt_secret
-      aws_lambda_api_gateway_endpoint = "${var.aws_lambda_api_gateway_endpoint}/${var.deployment_key}"
-      hasura_image                    = docker_registry_image.hasura.name
-      hasura_memory_credits           = var.hasura_memory_credits
-      hasura_cpu_credits              = var.hasura_cpu_credits
-      hasura_healthcheck_interval     = var.hasura_healthcheck_interval
-      hasura_healthcheck_retries      = var.hasura_healthcheck_retries
-
-      eod_websockets_memory_credits     = var.eod_websockets_memory_credits
-      polygon_websockets_memory_credits = var.polygon_websockets_memory_credits
-      websockets_image                  = docker_registry_image.websockets.name
-
-      env                                  = var.env
-      eodhistoricaldata_api_token          = var.eodhistoricaldata_api_token
-      eodhistoricaldata_exchanges          = jsonencode(["US", "CC", "INDX"])
-      pg_host                              = var.pg_host
-      pg_password                          = var.pg_password
-      pg_port                              = var.pg_port
-      pg_username                          = var.pg_username
-      pg_dbname                            = var.pg_dbname
-      pg_replica_uris                      = var.pg_replica_uris
-      pg_load_schema                       = "raw_data"
-      pg_transform_schema                  = local.public_schema_name
-      dbt_threads                          = var.env == "production" ? 4 : 4
-      pg_production_host                   = var.pg_production_host
-      pg_production_port                   = var.pg_production_port
-      pg_production_internal_sync_username = var.pg_production_internal_sync_username
-      pg_production_internal_sync_password = var.pg_production_internal_sync_password
-      pg_meltano_schema                    = "meltano"
-      pg_airflow_schema                    = "airflow"
-      airflow_password                     = random_password.airflow.result
-      meltano_image                        = docker_registry_image.meltano.name
-      aws_log_group_name                   = var.aws_log_group_name
-      aws_log_region                       = var.aws_log_region
-      airflow_port                         = 5001
-      eodhistoricaldata_jobs_count         = var.eodhistoricaldata_jobs_count
-      airflow_ui_memory_credits            = var.ui_memory_credits
-      airflow_scheduler_memory_credits     = var.scheduler_memory_credits
-      airflow_scheduler_cpu_credits        = var.scheduler_cpu_credits
-      algolia_tickers_index                = var.algolia_tickers_index
-      algolia_collections_index            = var.algolia_collections_index
-      algolia_app_id                       = var.algolia_app_id
-      algolia_indexing_key                 = var.algolia_indexing_key
-
-      pg_external_access_host     = var.pg_external_access_host
-      pg_external_access_port     = var.pg_external_access_port
-      pg_external_access_username = var.pg_external_access_username
-      pg_external_access_password = var.pg_external_access_password
-      pg_external_access_dbname   = var.pg_external_access_dbname
-      pg_analytics_schema         = var.pg_analytics_schema
-      pg_website_schema           = var.pg_website_schema
-
-      datadog_api_key = var.datadog_api_key
-      datadog_app_key = var.datadog_app_key
-
-      polygon_api_token               = var.polygon_api_token
-      polygon_realtime_streaming_host = "delayed.polygon.io" # socket.polygon.io for real-time
-
-      # mlflow
-      aws_region               = var.aws_region
-      aws_access_key           = var.aws_access_key
-      aws_secret_key           = var.aws_secret_key
-      mlflow_artifact_location = "s3://${var.mlflow_artifact_bucket}"
-      pg_mlflow_schema         = "mlflow"
-    }
-  )
-}
-
-resource "aws_iam_role" "hasura" {
-  name               = "ecsInstanceRole-gainy-fargate-hasura-${var.env}"
+resource "aws_iam_role" "execution" {
+  name               = "ecs-gainy-execution-role-${var.env}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -271,9 +253,9 @@ resource "aws_iam_role" "hasura" {
 EOF
 }
 
-resource "aws_iam_policy" "hasura_exec" {
-  name        = "hasura_${var.env}"
-  description = "Canary Exec Policy ${var.env}"
+resource "aws_iam_policy" "fargate_execution" {
+  name        = "ecs-gainy-execution-policy-${var.env}"
+  description = "ECS Gainy Exec Policy ${var.env}"
 
   policy = jsonencode({
     "Version" : "2012-10-17",
@@ -293,14 +275,36 @@ resource "aws_iam_policy" "hasura_exec" {
     ]
   })
 }
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_hasura_default" {
-  role       = aws_iam_role.hasura.name
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_default" {
+  role       = aws_iam_role.execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_hasura_custom" {
-  role       = aws_iam_role.hasura.name
-  policy_arn = aws_iam_policy.hasura_exec.arn
+resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_custom" {
+  role       = aws_iam_role.execution.name
+  policy_arn = aws_iam_policy.fargate_execution.arn
 }
+
+###### Create task definitions ######
+resource "aws_ecs_task_definition" "default" {
+  family                   = "gainy-${var.env}"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.main_cpu_credits
+  memory                   = var.main_memory_credits
+  tags                     = {}
+  execution_role_arn       = aws_iam_role.execution.arn
+
+  volume {
+    name = "meltano-data"
+  }
+
+  container_definitions = jsonencode([
+    local.hasura_task_description,
+    local.meltano_airflow_ui_task_description,
+    local.meltano_airflow_scheduler_description,
+  ])
+}
+
 resource "aws_ecs_task_definition" "hasura" {
   family                   = "gainy-hasura-${var.env}"
   network_mode             = "awsvpc"
@@ -308,12 +312,7 @@ resource "aws_ecs_task_definition" "hasura" {
   cpu                      = var.hasura_cpu_credits
   memory                   = var.hasura_memory_credits
   tags                     = {}
-  execution_role_arn       = aws_iam_role.hasura.arn
-
-  #  runtime_platform {
-  #    operating_system_family = "LINUX"
-  #    cpu_architecture        = "X86_64"
-  #  }
+  execution_role_arn       = aws_iam_role.execution.arn
 
   container_definitions = jsonencode([
     local.hasura_replica_task_description
