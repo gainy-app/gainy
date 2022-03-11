a3591aa710fe17ede68ba6a957539ded1cf8371f
Mikhail Astashkevich <asmisha@tut.by>
2022-03-11 06:49:41 +0400

    fix meltano rebuilds

--- terraform/aws/ecs/services/main.tf
+++ terraform/aws/ecs/services/main.tf
@@ -1,16 +1,18 @@
 locals {
   ecr_repo = var.repository_name
 
-  meltano_build_args = {
-    BASE_IMAGE_REGISTRY_ADDRESS = var.base_image_registry_address
+  meltano_build_args_force_build = {
     BASE_IMAGE_VERSION          = var.base_image_version
-    CODEARTIFACT_PIPY_URL       = var.codeartifact_pipy_url
     GAINY_COMPUTE_VERSION       = var.gainy_compute_version
     MELTANO_SOURCE_MD5          = data.archive_file.meltano_source.output_md5
   }
+  meltano_build_args = merge(local.meltano_build_args_force_build, {
+    BASE_IMAGE_REGISTRY_ADDRESS = var.base_image_registry_address
+    CODEARTIFACT_PIPY_URL       = var.codeartifact_pipy_url
+  })
 
   meltano_root_dir       = abspath("${path.cwd}/../src/gainy-fetch")
-  meltano_image_tag      = format("meltano-%s-%s-%s", var.env, var.base_image_version, md5(jsonencode(local.meltano_build_args)))
+  meltano_image_tag      = format("meltano-%s-%s-%s", var.env, var.base_image_version, md5(jsonencode(local.meltano_build_args_force_build)))
   meltano_ecr_image_name = format("%v/%v:%v", var.ecr_address, local.ecr_repo, local.meltano_image_tag)
 
   hasura_root_dir       = abspath("${path.cwd}/../src/hasura")
