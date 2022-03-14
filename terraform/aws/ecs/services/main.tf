bb78a8f3db7a516e117ee7dd67e550fda8a902c5
Mikhail Astashkevich <asmisha@tut.by>
2022-03-14 07:14:32 +0400

    fix

--- terraform/aws/ecs/services/main.tf
+++ terraform/aws/ecs/services/main.tf
@@ -5,7 +5,7 @@ resource "time_static" "meltano_changed" {
 }
 
 locals {
-  public_schema_name                = "public_${formatdate("YYMMDDhhmmss", time_static.meltano_changed.unix)}"
+  public_schema_name                = "public_${formatdate("YYMMDDhhmmss", time_static.meltano_changed.rfc3339)}"
   hasura_healthcheck_interval       = var.env == "production" ? 60 : 60
   hasura_healthcheck_retries        = var.env == "production" ? 3 : 3
   health_check_grace_period_seconds = var.env == "production" ? 60 * 10 : 60 * 20
