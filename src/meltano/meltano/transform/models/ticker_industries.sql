a9f3ec44ff4caa2fbbd697d0b13054ac18a68011
Mikhail Astashkevich <asmisha@tut.by>
2022-03-27 10:43:07 +0400

    fix industries and continuous interests

--- src/meltano/meltano/transform/models/ticker_industries.sql
+++ src/meltano/meltano/transform/models/ticker_industries.sql
@@ -17,13 +17,15 @@ with common_stocks as
              where type = 'common stock'
          ),
 
-     ticker_auto_industries
-         as ( -- union stack for the future: in case we would like more softeness on interests/industries we can use more top industries from the the model here
+     ticker_auto_industries as (
+         -- union stack for the future: in case we would like more softeness on interests/industries we can use more top industries from the the model here (by union that blocks)
+         -- 1st auto industry
          (select ati.symbol            as symbol,
                  ati.industry_id_1     as industry_id,
                  ati.industry_1_cossim as sim,
                  1::int                as industry_preorder
           from {{ source('gainy', 'auto_ticker_industries') }} ati)
+         -- 2nd auto industry
          union
          (select ati.symbol            as symbol,
                  ati.industry_id_2     as industry_id,
@@ -32,13 +34,16 @@ with common_stocks as
           from {{ source('gainy', 'auto_ticker_industries') }} ati)
      ),
 
+-- manual industries, don't touch it
      ticker_manual_industries as (
          select rgti.code  as symbol,
                 gi.id::int as industry_id,
-                1.1::float  as sim,              -- we completely sure about manual marked industries, sim=1.1
+                1.::float  as sim,              -- we completely sure about manual marked industries, sim=1.
                 0::int     as industry_preorder -- sim theoretically could be equal, so lets presave logical order to sort final industry order by this field
          from {{ source('gainy', 'gainy_ticker_industries') }} rgti -- 1 row
                   join {{ ref('gainy_industries') }} gi on gi."name" = rgti."industry name"
+                  join ticker_auto_industries ai on ai.symbol = rgti.code
+         group by rgti.code, gi.id::int
      ),
 
 -- step 1. overwrite similarity=1 and preorder=0 if manual industry is in auto industries of the ticker (making that manual one as top 1 industry)
@@ -66,38 +71,46 @@ with common_stocks as
 
 -- union 3 parts
      ticker_industries_union as (
-         (select miovai.*
-          from ticker_industries_mi_over_ai miovai -- 1. plain auto and manual overwrited auto excluding toshift part
-                   left join ticker_symbols_for_shift sfs on sfs.symbol = miovai.symbol
-          where sfs.symbol is null)
-         union
-         (select miovai.*
-          from ticker_industries_mi_over_ai miovai -- 2. toshift part 1: drop the last industry (industry_preorder < max(industry_preorder) (that "max" gives the actual length of the industries sequence that model saves to ticker_auto_industries, so we are able to set the model to output and save more lenghty industries sequences, to be able to get more softeness on interests/industries match component))
-                   join ticker_symbols_for_shift sfs on sfs.symbol = miovai.symbol
-          where miovai.industry_preorder < (select max(industry_preorder) from ticker_auto_industries))
-         -- if model somehow gives not length-equal sequences then we save here, coz for low-length sequences the manual industry will be just added without dropping the last
-         union
-         (select mi.* -- 3. toshift part 2: add 1 manual industry on top of the sequence (mi.industry_preorder = 0)
-          from ticker_manual_industries mi
-                   join ticker_symbols_for_shift sfs on sfs.symbol = mi.symbol)
+         with union3 as (
+             (select miovai.*
+              from ticker_industries_mi_over_ai miovai -- 1. plain auto and manual overwrited auto excluding toshift part
+                       left join ticker_symbols_for_shift sfs on sfs.symbol = miovai.symbol
+              where sfs.symbol is null)
+             union
+             (select miovai.*
+              from ticker_industries_mi_over_ai miovai -- 2. toshift part 1: drop the last industry (industry_preorder < max(industry_preorder) (that "max" gives the actual length of the industries sequence that model saves to ticker_auto_industries, so we are able to set the model to output and save more lenghty industries sequences, to be able to get more softeness on interests/industries match component))
+                       join ticker_symbols_for_shift sfs on sfs.symbol = miovai.symbol
+              where miovai.industry_preorder < (select max(industry_preorder) from ticker_auto_industries))
+             -- if model somehow gives not length-equal sequences then we save here, coz for low-length sequences the manual industry will be just added without dropping the last
+             union
+             (select mi.* -- 3. toshift part 2: add 1 manual industry on top of the sequence (mi.industry_preorder = 0)
+              from ticker_manual_industries mi
+                       join ticker_symbols_for_shift sfs on sfs.symbol = mi.symbol)
+         )
+         select u3.symbol,
+                industry_id, -- the more company diversified - the more it's recognizible by the people (and that approach really correlates with ranking from manual industries)
+                (sim * (1e-30 + avg(sim) over (partition by u3.symbol))) as sim,
+                industry_preorder
+         from union3 u3
      ),
 
-     ticker_maxmin_sim
-         as ( -- model classification generalization for each ticker: top 1st industry is max class for ticker, set is as 1.0 (all other classes for ticker must be normalized by /(max-min)=>E(f):(0..1.))
+     ticker_industries_nrmlzd as (
          select tiu.symbol,
-                max(tiu.sim)   as sim_max, -- manual sim overwrites model's max sim it's ok
-                ati.min_cossim as sim_min
+                tiu.industry_id,
+                (tiu.sim - (min(tiu.sim) over (partition by tiu.industry_id) - 1e-1)) -- inside each industry-"world" normalization (companies-involvement-wise)
+                    / (1e-30 +
+                       ((1e-1 + max(tiu.sim) over (partition by tiu.industry_id)) -- (we need this either for measure entrance into interest by company involvement in "world" of industry (which is assigned to interest) )
+                           - (min(tiu.sim) over (partition by tiu.industry_id) - 1e-1))
+                    ) as sim,
+                tiu.industry_preorder
          from ticker_industries_union tiu
-                  join {{ source('gainy', 'auto_ticker_industries') }} ati on ati.symbol = tiu.symbol
-         group by tiu.symbol, ati.min_cossim
      )
 
-select concat(tiu.symbol, '_', industry_id)::varchar                                  as id,
-       tiu.symbol,
-       tiu.industry_id,
-       (1e-30 + tiu.sim - tmms.sim_min) / (1e-30 + tmms.sim_max - tmms.sim_min)               as similarity, -- 1e-30 epsilon in both numerator and denominator
-       row_number() over (partition by tiu.symbol order by tiu.industry_preorder asc) as industry_order,
-       now()                                                                          as updated_at
-from ticker_industries_union tiu
-         join ticker_maxmin_sim tmms on tmms.symbol = tiu.symbol
-         join common_stocks cs on cs.symbol = tiu.symbol
+select concat(tin.symbol, '_', tin.industry_id)::varchar                                            as id,
+       tin.symbol,
+       tin.industry_id,
+       tin.sim                                                                                      as similarity,
+       row_number() over (partition by tin.symbol order by tin.sim desc, tin.industry_preorder asc) as industry_order,
+       now()                                                                                        as updated_at
+from ticker_industries_nrmlzd tin
+         join common_stocks cs on cs.symbol = tin.symbol
--- src/meltano/meltano/transform/models/ticker_interests.sql
+++ src/meltano/meltano/transform/models/ticker_interests.sql
@@ -10,9 +10,37 @@
   )
 }}
 
-SELECT distinct concat(ti.symbol, '_', ii.interest_id)::varchar as id,
-                ti.symbol,
+with common_stocks as
+         (
+             select *
+             from {{ ref('tickers') }}
+             where "type" = 'common stock'
+         ),
+
+     ticker_interest_similarity as (
+         select tind.symbol,
                 ii.interest_id,
-                now() as updated_at
-from {{ ref('interest_industries') }} ii
-    join {{ ref ('ticker_industries') }} ti on ii.industry_id = ti.industry_id
+                max(tind.similarity) as similarity -- entrance of ticker to interest by biggest industry that is listed in interest
+         from {{ ref ('ticker_industries') }} tind
+                  join {{ ref('interest_industries') }} ii on ii.industry_id = tind.industry_id
+         group by tind.symbol, ii.interest_id
+     ),
+
+     sim_dif as (
+         select tis.symbol,
+                tis.interest_id,
+                ((1e-30 + (similarity - (min(similarity) over (partition by tis.interest_id))))
+                     / (1e-30 + (max(similarity) over (partition by tis.interest_id) -
+                                 min(similarity) over (partition by tis.interest_id)))
+                    + 1e-2) /
+                (1. + 1e-2) as sim_dif -- [1e-2 .. [0.. 1-1e-2]] (inside-each-interest "world" normalized + 1e-2)
+         from ticker_interest_similarity tis
+     )
+
+select (sd.symbol || '_' || sd.interest_id)::varchar as id,
+       sd.symbol,
+       sd.interest_id,
+       sd.sim_dif,
+       now()::timestamp                              as updated_at
+from sim_dif sd
+         join common_stocks using (symbol)
