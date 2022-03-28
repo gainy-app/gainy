9602703b72d4425814c2610e0e2f58488d05a91e
Mikhail Astashkevich <asmisha@tut.by>
2022-03-28 18:29:32 +0400

    fix tests

--- src/hasura/hasura/seeds/00-profile.sql
+++ src/hasura/hasura/seeds/00-profile.sql
@@ -1,16 +1,16 @@
-with inserted_profile as
-         (INSERT INTO app.profiles (email, first_name, last_name, gender, created_at, user_id, avatar_url,
+INSERT INTO app.profiles (id, email, first_name, last_name, gender, created_at, user_id, avatar_url,
                                     legal_address)
-             VALUES ('test@gainy.app', 'fn', 'ln', 0, '2021-10-18 11:46:18.851570 +00:00',
+             VALUES (1, 'test@gainy.app', 'fn', 'ln', 0, '2021-10-18 11:46:18.851570 +00:00',
                      'AO0OQyz0jyL5lNUpvKbpVdAPvlI3', '',
-                     'legal_address')
-             RETURNING id)
-INSERT
-INTO app.profile_scoring_settings (profile_id, created_at, risk_level, average_market_return, investment_horizon,
+                     'legal_address'),
+                    (2, 'test2@gainy.app', 'fn', 'ln', 0, '2021-10-18 11:46:18.851570 +00:00',
+                     'AO0OQyz0jyL5lNUpvKbpVdAPvlI4', '',
+                     'legal_address');
+ALTER SEQUENCE app.profiles_id_seq RESTART WITH 3;
+
+INSERT INTO app.profile_scoring_settings (profile_id, created_at, risk_level, average_market_return, investment_horizon,
                                    unexpected_purchases_source, damage_of_failure, stock_market_risk_level,
                                    trading_experience, if_market_drops_20_i_will_buy,
                                    if_market_drops_40_i_will_buy)
-VALUES ((SELECT id FROM inserted_profile), '2021-10-18 11:46:18.851570 +00:00', 0.5, 6, 0.5, 'checking_savings', 0.5,
-        'very_risky',
-        'never_tried', 0.5,
-        0.5)
+VALUES (1, '2021-10-18 11:46:18.851570 +00:00', 0.5, 6, 0.5, 'checking_savings', 0.5, 'very_risky', 'never_tried', 0.5, 0.5),
+       (2, '2021-10-18 11:46:18.851570 +00:00', 0.5, 6, 0.5, 'checking_savings', 0.5, 'very_risky', 'never_tried', 0.5, 0.5);
