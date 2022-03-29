9e88fe5fbbb50c2a80c45e20a7ceb6104f4bf123
Mikhail Astashkevich <asmisha@tut.by>
2022-03-29 13:53:06 +0400

    fix chart and speed up historical_prices_aggregated and ticker_realtime_metrics

--- src/meltano/meltano/transform/models/ticker_realtime_metrics.sql
+++ src/meltano/meltano/transform/models/ticker_realtime_metrics.sql
@@ -10,46 +10,77 @@
 
 with latest_trading_day as
          (
-             select distinct on (symbol) symbol,
-                                         last_value(close)
-                                         over (partition by symbol order by datetime rows between current row and unbounded following) as close_price,
-                                         last_value(datetime)
-                                         over (partition by symbol order by datetime rows between current row and unbounded following) as close_datetime,
-                                         sum(volume)
-                                         over (partition by symbol order by datetime rows between current row and unbounded following) as volume
-             from {{ ref('chart') }}
-             where period = '1d'
-             order by symbol, datetime
+             select chart_open.symbol,
+                    chart_open.open    as open_price,
+                    chart_close.close    as close_price,
+                    t.min_datetime as open_datetime,
+                    t.max_datetime as close_datetime,
+                    t.sum_volume   as volume
+             from (
+                      select symbol,
+                             max(datetime) as max_datetime,
+                             min(datetime) as min_datetime,
+                             sum(volume)      sum_volume
+                      from {{ ref('chart') }}
+                      where period = '1d'
+                      group by symbol
+                  ) t
+                      join {{ ref('chart') }} chart_close on chart_close.symbol = t.symbol and chart_close.period = '1d' and chart_close.datetime = t.max_datetime
+                      join {{ ref('chart') }} chart_open on chart_open.symbol = t.symbol and chart_open.period = '1d' and chart_open.datetime = t.min_datetime
+         ),
+     previous_trading_day as
+         (
+             (
+                 select historical_prices.code as symbol,
+                        historical_prices.adjusted_close
+                 from (
+                          select historical_prices.code, max(historical_prices.date) as date
+                          from {{ ref('historical_prices') }}
+                                   join latest_trading_day
+                                        on historical_prices.code = latest_trading_day.symbol and
+                                           historical_prices.date < latest_trading_day.open_datetime::date
+                                   join {{ ref('base_tickers') }} on base_tickers.symbol = historical_prices.code
+                          where base_tickers.type != 'crypto'
+                          group by historical_prices.code
+                      ) t
+                          join {{ ref('historical_prices') }}
+                               on historical_prices.code = t.code and historical_prices.date = t.date
+             )
+             union all
+             (
+                 select latest_trading_day.symbol,
+                        latest_trading_day.open_price
+                 from latest_trading_day
+                          join {{ ref('base_tickers') }} using (symbol)
+                 where base_tickers.type = 'crypto'
+             )
          ),
      latest_realtime_datapoint as
          (
-             select distinct on (symbol) symbol,
-                                         close,
-                                         time
-             from {{ source('eod', 'eod_intraday_prices') }}
-             where time > now() - interval '1 week'
-             order by symbol, time desc
+             select eod_intraday_prices.symbol,
+                    eod_intraday_prices.close,
+                    eod_intraday_prices.time
+             from (
+                      select symbol,
+                             max(time) as time
+                      from {{ source('eod', 'eod_intraday_prices') }}
+                      group by symbol
+                  ) t
+                      join {{ source('eod', 'eod_intraday_prices') }}
+                           on eod_intraday_prices.symbol = t.symbol and eod_intraday_prices.time = t.time
          )
-select distinct on (
-    latest_trading_day.symbol
-    ) latest_trading_day.symbol,
-      latest_trading_day.close_datetime                                            as time,
-      latest_trading_day.close_price                                               as actual_price,
-      latest_trading_day.close_price - historical_prices_aggregated.adjusted_close as absolute_daily_change,
-      case
-          when historical_prices_aggregated.adjusted_close > 0
-              then (latest_trading_day.close_price / historical_prices_aggregated.adjusted_close) - 1
-          end                                                                      as relative_daily_change,
-      latest_trading_day.volume::double precision                                  as daily_volume,
-      latest_realtime_datapoint.close::double precision                            as last_known_price,
-      latest_realtime_datapoint.time::timestamp                                    as last_known_price_datetime,
-      historical_prices_aggregated.adjusted_close                                  as previous_day_close_price
+select latest_trading_day.symbol,
+       latest_trading_day.close_datetime                                    as time,
+       latest_trading_day.close_price                                       as actual_price,
+       latest_trading_day.close_price - previous_trading_day.adjusted_close as absolute_daily_change,
+       case
+           when previous_trading_day.adjusted_close > 0
+               then (latest_trading_day.close_price / previous_trading_day.adjusted_close) - 1
+           end                                                              as relative_daily_change,
+       latest_trading_day.volume::double precision                          as daily_volume,
+       latest_realtime_datapoint.close::double precision                    as last_known_price,
+       latest_realtime_datapoint.time::timestamp                            as last_known_price_datetime,
+       previous_trading_day.adjusted_close                                  as previous_day_close_price
 from latest_trading_day
-         join {{ ref('historical_prices_aggregated') }}
-              on historical_prices_aggregated.period = '1d'
-                  and historical_prices_aggregated.symbol = latest_trading_day.symbol
-                  and historical_prices_aggregated.datetime < latest_trading_day.close_datetime::date
-                  and historical_prices_aggregated.datetime > latest_trading_day.close_datetime - interval '1 week'
+         join previous_trading_day on previous_trading_day.symbol = latest_trading_day.symbol
          left join latest_realtime_datapoint on latest_realtime_datapoint.symbol = latest_trading_day.symbol
-         left join {{ ref('base_tickers') }} on base_tickers.symbol = latest_trading_day.symbol
-order by latest_trading_day.symbol, historical_prices_aggregated.datetime desc
