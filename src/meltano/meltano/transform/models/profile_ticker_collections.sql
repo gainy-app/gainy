8399a3aa9c98e8e06017d37a7ccd941aa55f34c3
Mikhail Astashkevich <asmisha@tut.by>
2022-04-30 17:25:16 +0400

    Improve relationship between collection and tickers

--- src/meltano/meltano/transform/models/profile_ticker_collections.sql
+++ src/meltano/meltano/transform/models/profile_ticker_collections.sql
@@ -7,10 +7,12 @@
 
 SELECT ptc.profile_id,
        ptc.collection_id,
+       (profile_id || '_' || collection_id)::varchar as collection_uniq_id,
        ptc.symbol
 FROM {{ source('app', 'personalized_ticker_collections') }} ptc
 UNION
 SELECT NULL :: integer AS profile_id,
        tc.collection_id,
+       ('0_' || collection_id)::varchar as collection_uniq_id,
        tc.symbol
 FROM {{ ref('ticker_collections') }} tc
