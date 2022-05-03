fdea97c00fbaa9f86e641318993913260c0a0bd9
Mikhail Astashkevich <asmisha@tut.by>
2022-05-03 11:33:08 +0400

    refactor dependancies

--- src/meltano/meltano/transform/models/historical_prices_aggregated.sql
+++ src/meltano/meltano/transform/models/historical_prices_aggregated.sql
@@ -505,37 +505,33 @@ union all
 (
     select DISTINCT ON (
         code,
-        date_truncated
-        ) (code || '_' || date_truncated || '_1w')::varchar                                                       as id,
+        date_week
+        ) (code || '_' || date_week || '_1w')::varchar                                                       as id,
           code as symbol,
-          date_truncated::timestamp                                                                               as time,
-          date_truncated::timestamp                                                                               as datetime,
-          '1w'::varchar                                                                                           as period,
+          date_week                                                                                          as time,
+          date_week                                                                                          as datetime,
+          '1w'::varchar                                                                                      as period,
           first_value(open)
-          OVER (partition by code, date_truncated order by date rows between current row and unbounded following) as open,
+          OVER (partition by code, date_week order by date rows between current row and unbounded following) as open,
           max(high)
-          OVER (partition by code, date_truncated rows between current row and unbounded following)               as high,
+          OVER (partition by code, date_week rows between current row and unbounded following)               as high,
           min(low)
-          OVER (partition by code, date_truncated rows between current row and unbounded following)               as low,
+          OVER (partition by code, date_week rows between current row and unbounded following)               as low,
           last_value(close)
-          OVER (partition by code, date_truncated order by date rows between current row and unbounded following) as close,
+          OVER (partition by code, date_week order by date rows between current row and unbounded following) as close,
           last_value(adjusted_close)
-          OVER (partition by code, date_truncated order by date rows between current row and unbounded following) as adjusted_close,
+          OVER (partition by code, date_week order by date rows between current row and unbounded following) as adjusted_close,
           sum(volume)
-          OVER (partition by code, date_truncated rows between current row and unbounded following)               as volume
-    from (
-             select historical_prices.*,
-                    date_trunc('week', date) as date_truncated
-             from {{ ref('historical_prices') }}
+          OVER (partition by code, date_week rows between current row and unbounded following)               as volume
+    from {{ ref('historical_prices') }}
 {% if is_incremental() %}
-             left join max_date on max_date.symbol = code and max_date.period = '1w'
-             where (max_date.time is null or date_trunc('week', date) >= max_date.time - interval '1 month')
-               and date >= now() - interval '5 year' - interval '1 week'
+    left join max_date on max_date.symbol = code and max_date.period = '1w'
+    where (max_date.time is null or date_trunc('week', date) >= max_date.time - interval '1 month')
+      and date >= now() - interval '5 year' - interval '1 week'
 {% else %}
-             where date >= now() - interval '5 year' - interval '1 week'
+    where date >= now() - interval '5 year' - interval '1 week'
 {% endif %}
-         ) t
-    order by symbol, date_truncated, date
+    order by symbol, date_week, date
 )
 
 union all
