0312f5e271302a4277f735d1a7bf3dd7b0f7842c
Mikhail Astashkevich <asmisha@tut.by>
2022-05-13 17:47:27 +0300

    fix dag

--- src/meltano/meltano/orchestrate/dags/send-notifications.py
+++ src/meltano/meltano/orchestrate/dags/send-notifications.py
@@ -3,9 +3,11 @@ from common import create_dag
 
 dag_id = "send-notifications"
 tags = ["notifications"]
-dag = create_dag(dag_id, tags=tags, schedule_interval="* * * * *")
+dag = create_dag(dag_id,
+                 tags=tags,
+                 is_paused_upon_creation=True,
+                 schedule_interval="* * * * *")
 
 operator = BashOperator(task_id="send-notifications",
                         bash_command="send_notifications.py",
-                        dag=dag,
-                        is_paused_upon_creation=True)
+                        dag=dag)
