31af3cebcf9583316ced14b6abfbfb4d30492236
Mikhail Astashkevich <asmisha@tut.by>
2022-05-18 13:06:33 +0300

    Collection MS explanation: new algorithm

--- src/meltano/meltano/transform/models/collections/collection_match_score_explanation.sql
+++ src/meltano/meltano/transform/models/collections/collection_match_score_explanation.sql
@@ -6,76 +6,114 @@
 }}
 
 
-with collection_categories as
+with t_cat_sim_dif as
          (
-             select *
-             from (
-                      select *,
-                             row_number() over (partition by profile_id, collection_id order by cnt desc) as row_num
-                      from (
-                               select profile_id,
-                                      user_id,
-                                      collection_id,
-                                      category_match_id,
-                                      count(*) as cnt
-                               from (
-                                        select profile_id,
-                                               user_id,
-                                               collection_id,
-                                               json_array_elements_text(category_matches::json)::int as category_match_id
-                                        from {{ ref('ticker_collections') }}
-                                                 join {{ source('app', 'profile_ticker_match_score') }} using (symbol)
-                                                 join {{ source('app', 'profiles') }} on profiles.id = profile_id
-                                    ) t
-                               group by profile_id, user_id, collection_id, category_match_id
-                           ) t
-                  ) t
-             where row_num <= 3
+             select category_id, symbol, sim_dif / 2 + 0.5 as sim_dif
+             from {{ ref('ticker_categories_continuous') }}
          ),
-     collection_interests as
+     t_int_sim_dif as
          (
-             select *
+             select interest_id, symbol, sim_dif / 2 + 0.5 as sim_dif
+             from {{ ref('ticker_interests') }}
+         ),
+     profile_collection_categories as
+         (
+             select t.profile_id,
+                    t.collection_id,
+                    t.collection_uniq_id,
+                    t.category_id,
+                    t.match_score * weight as match_score
              from (
-                      select *,
-                             row_number() over (partition by profile_id, collection_id order by cnt desc) as row_num
-                      from (
-                               select profile_id,
-                                      user_id,
-                                      collection_id,
-                                      interest_match_id,
-                                      count(*) as cnt
-                               from (
-                                        select profile_id,
-                                               user_id,
-                                               collection_id,
-                                               json_array_elements_text(interest_matches::json)::int as interest_match_id
-                                        from {{ ref('ticker_collections') }}
-                                                 join {{ source('app', 'profile_ticker_match_score') }} using (symbol)
-                                                 join {{ source('app', 'profiles') }} on profiles.id = profile_id
-                                    ) t
-                               group by profile_id, user_id, collection_id, interest_match_id
-                           ) t
+                      select profile_ticker_match_score.profile_id,
+                             collection_tickers_weighted.collection_id,
+                             collection_tickers_weighted.collection_uniq_id,
+                             t_cat_sim_dif.category_id,
+                             sum(match_score * sim_dif) / sum(sim_dif) as match_score
+                      from {{ source('app', 'profile_ticker_match_score')}}
+                               join {{ ref('collection_tickers_weighted') }}
+                                    on collection_tickers_weighted.symbol = profile_ticker_match_score.symbol
+                                        and (collection_tickers_weighted.profile_id is null or
+                                             collection_tickers_weighted.profile_id =
+                                             profile_ticker_match_score.profile_id)
+                               join t_cat_sim_dif on t_cat_sim_dif.symbol = profile_ticker_match_score.symbol
+                      group by profile_ticker_match_score.profile_id,
+                               collection_tickers_weighted.collection_id,
+                               collection_tickers_weighted.collection_uniq_id,
+                               category_id
                   ) t
-             where row_num <= 3
+                      join {{ ref('collection_piechart') }}
+                           on collection_piechart.collection_id = t.collection_id
+                               and (collection_piechart.profile_id = t.profile_id or
+                                    collection_piechart.profile_id is null)
+                               and collection_piechart.entity_type = 'category'
+                               and collection_piechart.entity_id = category_id::varchar
+         ),
+     profile_collection_interests as
+         (
+             select t.profile_id,
+                    t.collection_id,
+                    t.collection_uniq_id,
+                    t.interest_id,
+                    t.match_score * weight as match_score
+             from (
+                      select profile_ticker_match_score.profile_id,
+                             collection_tickers_weighted.collection_id,
+                             collection_tickers_weighted.collection_uniq_id,
+                             t_int_sim_dif.interest_id,
+                             sum(match_score * sim_dif) / sum(sim_dif) as match_score
+                      from {{ source('app', 'profile_ticker_match_score')}}
+                               join {{ ref('collection_tickers_weighted') }}
+                                    on collection_tickers_weighted.symbol = profile_ticker_match_score.symbol
+                                        and (collection_tickers_weighted.profile_id is null or
+                                             collection_tickers_weighted.profile_id =
+                                             profile_ticker_match_score.profile_id)
+                               join t_int_sim_dif on t_int_sim_dif.symbol = profile_ticker_match_score.symbol
+                      group by profile_ticker_match_score.profile_id,
+                               collection_tickers_weighted.collection_id,
+                               collection_tickers_weighted.collection_uniq_id,
+                               interest_id
+                  ) t
+                      join {{ ref('collection_piechart') }}
+                           on collection_piechart.collection_id = t.collection_id
+                               and (collection_piechart.profile_id = t.profile_id or
+                                    collection_piechart.profile_id is null)
+                               and collection_piechart.entity_type = 'interest'
+                               and collection_piechart.entity_id = interest_id::varchar
+--              where weight > 0.2
+         ),
+     profile_collection_matches as
+         (
+             select t.*,
+                    user_id
+             from (
+                      select profile_id,
+                             collection_id,
+                             collection_uniq_id,
+                             match_score,
+                             category_id,
+                             null as interest_id
+                      from profile_collection_categories
+                      union all
+                      select profile_id,
+                             collection_id,
+                             collection_uniq_id,
+                             match_score,
+                             null as category_id,
+                             interest_id
+                      from profile_collection_interests
+                  ) t
+                      join {{ source('app', 'profiles')}} on profiles.id = t.profile_id
+         ),
+     profile_collection_matches_ranked as
+         (
+             select profile_collection_matches.*,
+                    row_number() over (partition by profile_id, collection_id order by match_score desc) as row_num
+             from profile_collection_matches
          )
-select t.*,
-       uniq_id as collection_uniq_id
-from (
-         select profile_id,
-                user_id,
-                collection_id,
-                category_match_id::int as category_id,
-                null                   as interest_id
-         from collection_categories
-         union all
-         select profile_id,
-                user_id,
-                collection_id,
-                null                   as category_id,
-                interest_match_id::int as interest_id
-         from collection_interests
-     ) t
-         join {{ ref('profile_collections') }}
-              on (profile_collections.profile_id is null or
-                  profile_collections.profile_id = t.profile_id)
-                  and profile_collections.id = collection_id
+select profile_id,
+       collection_id,
+       collection_uniq_id,
+       category_id,
+       interest_id
+from profile_collection_matches_ranked
+where row_num <= 5
