212a44e91a67edac367425595b7e0300efc57ea3
Mikhail Astashkevich <asmisha@tut.by>
2022-05-19 10:28:17 +0300

    Revert rules from restore-views Dag

--- src/meltano/meltano/orchestrate/dags/debug-restore-views-dag.py
+++ src/meltano/meltano/orchestrate/dags/debug-restore-views-dag.py
@@ -5,15 +5,7 @@ dag_id = "debug-restore-views"
 tags = ["meltano", "postgres", "dbt"]
 dag = create_dag(dag_id, tags=tags)
 
-generate_rules = BashOperator(
-    task_id="generate_rules",
-    bash_command=
-    f"cd {MELTANO_PROJECT_ROOT}; ( cd scripts && /usr/local/bin/python3 generate_rules_sql.py )",
-    dag=dag)
-
 dbt = BashOperator(
     task_id="dbt",
     bash_command=get_meltano_command("invoke dbt run --model tag:view"),
     dag=dag)
-
-generate_rules >> dbt
