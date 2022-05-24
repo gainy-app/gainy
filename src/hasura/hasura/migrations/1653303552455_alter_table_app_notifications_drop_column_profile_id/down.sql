203cd134e4a2009a9808ee36a7139d6b1bf70291
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-05-24 16:49:10 +0300

    Revert "Push notifications grouping (#451)"
    This reverts commit 1e76ad88a6613afadfbb2617051efe427e23eb4d.

--- src/hasura/hasura/migrations/1653303552455_alter_table_app_notifications_drop_column_profile_id/down.sql
+++
@@ -1,9 +0,0 @@
-alter table "app"."notifications"
-  add constraint "notifications_profile_id_fkey"
-  foreign key (profile_id)
-  references "app"."profiles"
-  (id) on update cascade on delete cascade;
-alter table "app"."notifications" alter column "profile_id" drop not null;
-alter table "app"."notifications" add column "profile_id" int4;
-
-alter table "app"."notifications" drop column "emails" cascade;
--- src/meltano/meltano/transform/models/push_notifications.sql
+++ src/meltano/meltano/transform/models/push_notifications.sql
@@ -10,97 +10,54 @@
 -- 6 biggest changes in your portfolio[better do it after market opens ~10am EST]
 -- Two separate notifications
 -- / Top losers: +3% TSLA, +5% MDB ...
-with individual_notifications as
-         (
-             select email,
-                    ('top_gainers_' || (string_agg(symbol, '_' order by relative_daily_change desc, symbol)) ||
-                     '_' || now()::date)::varchar                                                                 as uniq_id,
-                    min(exchange_schedule.open_at) + interval '30 minutes'                                        as send_at,
-                    json_build_object('en', 'Top gainers: ' ||
-                                            (string_agg(text, ', ' order by relative_daily_change desc, symbol))) as text,
-                    json_build_object('t', 0)                                                                     as data
-             from (
-                      select relative_daily_change,
-                             email,
-                             symbol,
-                             '+' || round(relative_daily_change * 100) || '% ' || symbol as text
-                      from {{ source('app', 'profiles')}}
-                               join {{ ref('profile_collection_tickers_performance_ranked') }}
-                                    on profile_collection_tickers_performance_ranked.profile_id = profiles.id
-                                        and gainer_rank <= 3
-                      where relative_daily_change > 0.01
-                  ) t
-                      join {{ ref('exchange_schedule') }} on exchange_schedule.country_name = 'USA' and
-                                                exchange_schedule.date = now()::date
-             where now() > exchange_schedule.open_at
-             group by email
+select profile_id,
+       (profile_id || '_top_gainers_' || now()::date)::varchar            as uniq_id,
+       min(exchange_schedule.open_at) + interval '30 minutes'             as send_at,
+       json_build_object('en', 'Top gainers: ' || string_agg(text, ', ')) as text,
+       json_build_object('t', 0)                                          as data
+from (
+         select profile_id, email, '+' || round(relative_daily_change * 100) || '% ' || symbol as text
+         from {{ source('app', 'profiles')}}
+                  join {{ ref('profile_collection_tickers_performance_ranked') }}
+                       on profile_collection_tickers_performance_ranked.profile_id = profiles.id
+                           and gainer_rank <= 3
+         where relative_daily_change > 0.01
+         order by relative_daily_change desc
+     ) t
+         join {{ ref('exchange_schedule') }} on exchange_schedule.country_name = 'USA' and exchange_schedule.date = now()::date
+where now() > exchange_schedule.open_at
+group by profile_id
 
-             union all
+union all
 
-             select email,
-                    ('top_losers_' || (string_agg(symbol, '_' order by relative_daily_change, symbol)) ||
-                     '_' || now()::date)::varchar                                                            as uniq_id,
-                    min(exchange_schedule.open_at) + interval '30 minutes'                                   as send_at,
-                    json_build_object('en', 'Top losers: ' ||
-                                            (string_agg(text, ', ' order by relative_daily_change, symbol))) as text,
-                    json_build_object('t', 0)                                                                as data
-             from (
-                      select relative_daily_change,
-                             email,
-                             symbol,
-                             round(relative_daily_change * 100) || '% ' || symbol as text
-                      from {{ source('app', 'profiles')}}
-                               join {{ ref('profile_collection_tickers_performance_ranked') }}
-                                    on profile_collection_tickers_performance_ranked.profile_id = profiles.id
-                                        and loser_rank <= 3
-                      where relative_daily_change < -0.01
-                  ) t
-                      join {{ ref('exchange_schedule') }} on exchange_schedule.country_name = 'USA' and
-                                                exchange_schedule.date = now()::date
-             where now() > exchange_schedule.open_at
-             group by email
-         ),
-     grouped_notifications as
-         (
-             select json_agg(email)       as emails,
-                    uniq_id               as original_uniq_id,
-                    uniq_id || '_' || grp as uniq_id,
-                    min(send_at)          as send_at
-             from (
-                      select *,
-                             ((row_number() over (partition by uniq_id) - 1) / 100)::int as grp -- max 100 emails in group
-                      from individual_notifications
-                  ) t
-             group by uniq_id, grp
-         ),
-     distinct_notifications as
-         (
-             select distinct on (
-                 uniq_id
-                 ) uniq_id,
-                   text,
-                   data
-             from individual_notifications
-         )
-select emails,
-       grouped_notifications.uniq_id::varchar,
-       send_at,
-       text,
-       data
-from grouped_notifications
-         join distinct_notifications
-              on grouped_notifications.original_uniq_id = distinct_notifications.uniq_id
+select profile_id,
+       (profile_id || '_top_losers_' || now()::date)::varchar            as uniq_id,
+       min(exchange_schedule.open_at) + interval '30 minutes'    as send_at,
+       json_build_object('en', 'Top losers: ' || string_agg(text, ', ')) as text,
+       json_build_object('t', 0)                                         as data
+from (
+         select profile_id, email, round(relative_daily_change * 100) || '% ' || symbol as text
+         from {{ source('app', 'profiles')}}
+                  join {{ ref('profile_collection_tickers_performance_ranked') }}
+                       on profile_collection_tickers_performance_ranked.profile_id = profiles.id
+                           and loser_rank <= 3
+         where relative_daily_change < -0.01
+         order by relative_daily_change desc
+     ) t
+         join {{ ref('exchange_schedule') }} on exchange_schedule.country_name = 'USA' and exchange_schedule.date = now()::date
+where now() > exchange_schedule.open_at
+group by profile_id
 
 union all
 
 -- Most performing TTF daily in all TTFs
 -- 1) MIN 3% diff for TTF to change 2) #1 TTF
 -- Notify with the performance of the top TTF on the platform  [better do it aftermarket opens and trades a bit ~12pm EST]
-select null                                                       as emails,
+select null                                                       as profile_id,
        ('top_ttf_' || now()::date)::varchar                       as uniq_id,
        exchange_schedule.open_at + interval '2 hours 30 minutes'  as send_at,
        json_build_object('en', 'The most performing TTF today is ' || collection_name || ' +' || round(relative_daily_change * 100) ||
-       '%. Check it out!')                                        as text,
+       '%. Check this out!')                                      as text,
        json_build_object('t', 1, 'ttf_id', collection_id)         as data
 from (
          select collection_uniq_id,
