fafeef4145a8ab54e4804a3b2331a319be28b00b
Mikhail Astashkevich <asmisha@tut.by>
2022-05-24 10:39:07 +0300

    Fix TF deprecated configuration

--- terraform/algolia/providers.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    algolia = {
-      source = "philippe-vandermoere/algolia"
-    }
-  }
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,10 @@ terraform {
 
 terraform {
   required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 2.0"
@@ -18,6 +22,22 @@ terraform {
     algolia = {
       source = "philippe-vandermoere/algolia"
     }
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "this" {}
+data "aws_ecr_authorization_token" "token" {}
+
+locals {
+  ecr_address = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name)
+}
+
+provider "docker" {
+  registry_auth {
+    address  = local.ecr_address
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
   }
 }
 
