ae4eee9c9d84b9f37bce14160a3bfa34bd2cdee6
Mikhail Astashkevich <asmisha@tut.by>
2022-06-07 12:06:38 +0300

    fix

--- src/aws/lambda-python/lambda_python/tests/actions/test_update_purchases.py
+++ src/aws/lambda-python/lambda_python/tests/actions/test_update_purchases.py
@@ -10,9 +10,8 @@ from actions import UpdatePurchases
 
 def test_get_duration():
     action = UpdatePurchases()
-    assert datetime.timedelta(days=int(1)) == action.get_duration('foo_d1')
-    assert datetime.timedelta(days=int(2) *
-                              7) == action.get_duration('foo_bar_w2')
+    assert datetime.timedelta(days=1) == action.get_duration('foo_d1')
+    assert datetime.timedelta(days=14) == action.get_duration('foo_bar_w2')
     assert dateutil.relativedelta.relativedelta(
         months=3) == action.get_duration('foo_m3_bar')
     assert dateutil.relativedelta.relativedelta(
