d70706f7dfb7dc95495f9bbea6f6fd35ec68e3d2
Mikhail Astashkevich <asmisha@tut.by>
2022-06-08 17:29:44 +0300

    Fix portfolio_transaction_chart

--- src/meltano/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
+++ src/meltano/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
@@ -17,22 +17,13 @@
 }}
 
 
+-- Execution Time: 108162.002 ms
 with first_profile_transaction_date as
          (
              select profile_id,
                     min(date) as datetime
              from {{ source('app', 'profile_portfolio_transactions') }}
              group by profile_id
-{% if is_incremental() %}
-         ),
-     latest_transaction_chart_row as
-         (
-             select transactions_uniq_id,
-                    period,
-                    max(datetime) as datetime
-             from {{ this }}
-             group by transactions_uniq_id, period
-{% endif %}
          )
 select (portfolio_expanded_transactions.uniq_id || '_' || chart.datetime || '_' || chart.period)::varchar   as id,
        portfolio_expanded_transactions.uniq_id                                                              as transactions_uniq_id,
@@ -44,22 +35,11 @@ select (portfolio_expanded_transactions.uniq_id || '_' || chart.datetime || '_' 
        portfolio_expanded_transactions.quantity_norm_for_valuation::numeric * chart.close::numeric          as close,
        portfolio_expanded_transactions.quantity_norm_for_valuation::numeric * chart.adjusted_close::numeric as adjusted_close
 from {{ ref('portfolio_expanded_transactions') }}
-         left join first_profile_transaction_date
-                   on first_profile_transaction_date.profile_id = portfolio_expanded_transactions.profile_id
-         join {{ ref('portfolio_securities_normalized') }}
-              on portfolio_securities_normalized.id = portfolio_expanded_transactions.security_id
-{% if is_incremental() %}
-         left join latest_transaction_chart_row
-              on latest_transaction_chart_row.transactions_uniq_id = portfolio_expanded_transactions.uniq_id
-{% endif %}
-         join {{ ref('chart') }}
-              on chart.symbol = portfolio_securities_normalized.original_ticker_symbol
-                  and (chart.datetime >= portfolio_expanded_transactions.date or portfolio_expanded_transactions.date is null)
-                  and (chart.datetime >= first_profile_transaction_date.datetime or first_profile_transaction_date.profile_id is null)
-{% if is_incremental() %}
-                  and (chart.period = latest_transaction_chart_row.period or latest_transaction_chart_row.transactions_uniq_id is null)
-                  and (chart.datetime >= latest_transaction_chart_row.datetime or latest_transaction_chart_row.transactions_uniq_id is null)
-{% endif %}
-{% if is_incremental() and var('realtime') %}
-where (chart.period in ('1d', '1w') or latest_transaction_chart_row.datetime is null)
-{% endif %}
+    left join first_profile_transaction_date
+on first_profile_transaction_date.profile_id = portfolio_expanded_transactions.profile_id
+    join {{ ref('portfolio_securities_normalized') }}
+    on portfolio_securities_normalized.id = portfolio_expanded_transactions.security_id
+    join {{ ref('chart') }}
+    on chart.symbol = portfolio_securities_normalized.original_ticker_symbol
+    and (chart.datetime >= portfolio_expanded_transactions.date or portfolio_expanded_transactions.date is null)
+    and (chart.datetime >= first_profile_transaction_date.datetime or first_profile_transaction_date.profile_id is null)
