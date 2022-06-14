72cf4eda23f087eb85e59a9e786d7d4cf381f2b0
Mikhail Astashkevich <asmisha@tut.by>
2022-06-14 17:23:51 +0200

    fix dag

--- src/meltano/meltano/orchestrate/dags/update-recommendations.py
+++ src/meltano/meltano/orchestrate/dags/update-recommendations.py
@@ -6,11 +6,10 @@ tags = ["gainy-compute", "debug", "recommendations"]
 dag = create_dag(
     dag_id,
     tags=tags,
+    is_paused_upon_creation=(ENV != 'production'),
     schedule_interval="0 4 * * 0" if ENV == "production" else "0 5 * * 0")
 
-gainy_recommendation = BashOperator(
-    task_id="update-recommendations",
-    bash_command="gainy_recommendation",
-    dag=dag,
-    pool="gainy_recommendation",
-    is_paused_upon_creation=(ENV != 'production'))
+gainy_recommendation = BashOperator(task_id="update-recommendations",
+                                    bash_command="gainy_recommendation",
+                                    dag=dag,
+                                    pool="gainy_recommendation")
