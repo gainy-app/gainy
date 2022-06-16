317d02886087976ee63b82987c4d5e60e3114371
Mikhail Astashkevich <asmisha@tut.by>
2022-06-16 11:17:37 +0200

    fix

--- src/meltano/meltano/orchestrate/dags/gainy-dag-weekly.py
+++ src/meltano/meltano/orchestrate/dags/gainy-dag-weekly.py
@@ -9,15 +9,14 @@ dag = create_dag(
     is_paused_upon_creation=(ENV != 'production'),
     schedule_interval="0 4 * * 0" if ENV == "production" else "0 5 * * 0")
 
-gainy_recommendation = BashOperator(
-    task_id="update-recommendations",
-    bash_command="gainy_recommendation",
-    dag=dag,
-    pool="gainy_recommendation")
+gainy_recommendation = BashOperator(task_id="update-recommendations",
+                                    bash_command="gainy_recommendation",
+                                    dag=dag,
+                                    pool="gainy_recommendation")
 
-upload_to_s3 = BashOperator(
-    task_id="postgres-history-weekly-to-s3",
-    bash_command=get_meltano_command("schedule run postgres-history-weekly-to-s3"),
-    dag=dag)
+upload_to_s3 = BashOperator(task_id="postgres-history-weekly-to-s3",
+                            bash_command=get_meltano_command(
+                                "schedule run postgres-history-weekly-to-s3"),
+                            dag=dag)
 
 gainy_recommendation >> upload_to_s3
