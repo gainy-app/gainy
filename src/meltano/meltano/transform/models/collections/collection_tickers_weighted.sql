9ece7a765b3da7d49c00e7d8363e87c82df28506
Mikhail Astashkevich <asmisha@tut.by>
2022-06-17 14:23:39 +0200

    Exclude test users from reporting + fixes

--- src/meltano/meltano/transform/models/collections/collection_tickers_weighted.sql
+++ src/meltano/meltano/transform/models/collections/collection_tickers_weighted.sql
@@ -6,34 +6,25 @@
 }}
 
 
-with all_ticker_collections as
-         (
-             select profile_id, collection_id, (profile_id || '_' || collection_id)::varchar AS collection_uniq_id, symbol, null as weight
-             from {{ source('app', 'personalized_ticker_collections') }}
-             union all
-             select null, collection_id, ('0_' || collection_id)::varchar AS collection_uniq_id, symbol, weight
-             from {{ ref('ticker_collections') }}
-         ),
-    weighted_ticker_collections as
-         (
-             select collection_id, sum(weight) as weight_sum
-             from {{ ref('ticker_collections') }}
-             group by collection_id
-         )
-select now()::date                as date,
-       all_ticker_collections.profile_id,
-       all_ticker_collections.collection_id,
-       collection_uniq_id,
+select now()::date           as date,
+       null                  as profile_id,
+       collection_id,
+       '0_' || collection_id as collection_uniq_id,
        symbol,
-       (case
-            when weighted_ticker_collections.weight_sum is null or weighted_ticker_collections.weight_sum < 1e-3
-                then
-                    ticker_metrics.market_capitalization /
-                    sum(ticker_metrics.market_capitalization) over (partition by collection_uniq_id)
-            else
-                weight / weighted_ticker_collections.weight_sum
-           end)::double precision as weight
-from all_ticker_collections
+       weight
+from {{ ref('static_collection_tickers_weighted') }}
+
+union all
+
+select now()::date                        as date,
+       profile_id,
+       collection_id,
+       profile_id || '_' || collection_id as collection_uniq_id,
+       symbol,
+       (
+               ticker_metrics.market_capitalization /
+               sum(ticker_metrics.market_capitalization) over (partition by profile_id, collection_id)
+           )::double precision            as weight
+from {{ source('app', 'personalized_ticker_collections') }}
          join {{ ref('ticker_metrics') }} using (symbol)
-         join weighted_ticker_collections using (collection_id)
 where ticker_metrics.market_capitalization is not null
