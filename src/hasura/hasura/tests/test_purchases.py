eec85d1d1f298fbe957fcc39b71df7c692a51ee8
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-06-17 16:49:22 +0300

    Grant promotion to redeemer of invitation (#484)
    * Grant promotion to redeemer of promocode
    
    * fix

--- src/hasura/hasura/tests/test_purchases.py
+++ src/hasura/hasura/tests/test_purchases.py
@@ -32,6 +32,12 @@ def test_invitations():
     assert profile_data['id'] == from_profile_id
     assert profile_data['subscription_end_date'] is not None
 
+    profile_data = make_graphql_request(
+        "{app_profiles{id, subscription_end_date}}", {},
+        to_profile_user_id)['data']['app_profiles'][0]
+    assert profile_data['id'] == to_profile_id
+    assert profile_data['subscription_end_date'] is not None
+
 
 def test_promocodes():
     query = "query GetPromocode($code: String!) { get_promocode(code: $code) { id name description config } }"
