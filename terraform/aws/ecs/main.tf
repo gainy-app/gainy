2a1a775546400e770458898ef58ac9a353504f27
Mikhail Astashkevich <asmisha@tut.by>
2022-06-17 15:58:16 +0200

    fix

--- terraform/aws/ecs/main.tf
+++ terraform/aws/ecs/main.tf
@@ -218,12 +218,12 @@ resource "aws_security_group" "public_http" {
   description = "Allow HTTP traffic from public"
   vpc_id      = aws_vpc.vpc.id
 
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+  #  ingress {
+  #    from_port   = 80
+  #    to_port     = 80
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
 }
 
 /*
