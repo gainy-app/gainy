f4e68749fde2823686e6f84fe2d2bd4465c3776e
Mikhail Astashkevich <asmisha@tut.by>
2022-06-17 12:09:35 +0200

    wip

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -6,23 +6,27 @@ const cors = require('cors')({ origin: true })
 
 admin.initializeApp(functions.config().firebase)
 
-const updateClaims = (uid) => admin.auth().setCustomUserClaims(uid, {
-  'https://hasura.io/jwt/claims': {
-    'x-hasura-default-role': 'user',
-    'x-hasura-allowed-roles': ['user'],
-    'x-hasura-user-id': uid,
-  },
-})
+const updateClaims = (uid, isAnonymous) => {
+  const role = isAnonymous ? 'anonymous' : 'user';
+
+  admin.auth().setCustomUserClaims(uid, {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': role,
+      'x-hasura-allowed-roles': [role],
+      'x-hasura-user-id': uid,
+    },
+  });
+}
 
 exports.processSignUp = functions.auth.user().onCreate((user) => {
-  console.log('ON CREATE', user.uid)
-  updateClaims(user.uid)
+  console.log('ON CREATE', user)
+  updateClaims(user.uid, user.sign_in_provider);
 })
 
 exports.refreshToken = functions.https.onRequest((req, res) => {
-  console.log('TOKEN REFRESH', req.query.uid)
+  console.log('TOKEN REFRESH', req.query)
   cors(req, res, () => {
-    updateClaims(req.query.uid).then(() => {
+    updateClaims(req.query.uid, req.query.isAnonymous).then(() => {
       res.status(200).send('success')
     }).catch((error) => {
       console.error('REFRESH ERROR', error)
