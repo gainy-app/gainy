d3bf4bbc94859e6f78e3c458f0aa6703a4f1ca2c
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-06-18 08:17:59 +0300

    Exclude test users from jobs + fixes (#483)
    * fixes and improvements
    
    * Exclude test users from reporting + fixes
    
    * cleanup
    
    * fix
    
    * fix tf
    
    * fix
    
    * fix
    
    * fix

--- src/meltano/meltano/transform/models/collections/ticker_collections.sql
+++ src/meltano/meltano/transform/models/collections/ticker_collections.sql
@@ -12,13 +12,38 @@
   )
 }}
 
-SELECT distinct (ticker_collections.symbol || '_' || collections.id::varchar)::varchar as id,
-                tickers.symbol,
-                collections.id as collection_id,
-                ticker_collections.weight::float,
-                NOW() as updated_at
-from {{ source('gainy', 'ticker_collections') }}
-         join {{ ref('tickers') }} on tickers.symbol = ticker_collections.symbol
-         join {{ ref('collections') }} on collections.name = trim(ticker_collections.ttf_name)
-where collections.personalized = '0'
-  and ticker_collections._sdc_extracted_at > (select max(_sdc_extracted_at) from {{ source ('gainy', 'ticker_collections') }}) - interval '1 minute'
+    
+with raw_ticker_collections as 
+         (
+             SELECT distinct (ticker_collections.symbol || '_' || collections.id::varchar)::varchar as id,
+                             tickers.symbol,
+                             collections.id as collection_id,
+                             ticker_collections.weight::double precision
+             from {{ source('gainy', 'ticker_collections') }}
+                      join {{ ref('tickers') }} on tickers.symbol = ticker_collections.symbol
+                      join {{ ref('collections') }} on collections.name = trim(ticker_collections.ttf_name)
+             where collections.personalized = '0'
+               and ticker_collections._sdc_extracted_at > (select max(_sdc_extracted_at) from {{ source ('gainy', 'ticker_collections') }}) - interval '1 minute'
+         ),
+     collection_weight_sum as
+         (
+             select collection_id, sum(weight) as weight_sum
+             from raw_ticker_collections
+             group by collection_id
+         )
+select collection_id || '_' || symbol as id,
+       collection_id,
+       symbol,
+       (case
+            when collection_weight_sum.weight_sum is null or collection_weight_sum.weight_sum < 1e-3
+                then
+                    ticker_metrics.market_capitalization /
+                    sum(ticker_metrics.market_capitalization) over (partition by collection_id)
+            else
+                weight / collection_weight_sum.weight_sum
+           end)::double precision     as weight,
+       now()                          as updated_at
+from raw_ticker_collections
+         join {{ ref('ticker_metrics') }} using (symbol)
+         join collection_weight_sum using (collection_id)
+where ticker_metrics.market_capitalization is not null
