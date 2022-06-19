309575a4ca90496617cb9b089bab0f82c39f6888
Mikhail Astashkevich <asmisha@tut.by>
2022-06-19 08:49:41 +0200

    Use functions to interpolate prices in historical_prices

--- src/meltano/meltano/transform/models/historical_prices/historical_prices_aggregated.sql
+++ src/meltano/meltano/transform/models/historical_prices/historical_prices_aggregated.sql
@@ -386,178 +386,107 @@ union all
 -- 1d
 -- Execution Time: 96290.198 ms
 (
-    with combined_daily_prices as
+    with uniq_tickers as
              (
-                 select DISTINCT ON (
-                     t.symbol,
-                     t.date
-                     ) t.*
-                 from (
-                         (
-                             select code as symbol,
-                                    date,
-                                    open,
-                                    high,
-                                    low,
-                                    close,
-                                    adjusted_close,
-                                    volume,
-                                    0 as priority
-                             from {{ ref('historical_prices') }}
-                             where date >= now() - interval '1 year' - interval '1 week'
-                         )
-                         union all
-                         (
-                             with filtered_base_tickers as
-                                  (
-                                      select symbol, exchange_canonical from {{ ref('base_tickers') }} where exchange_canonical is not null
-                                      union all
-                                      select contract_name as symbol, exchange_canonical
-                                      from {{ ref('ticker_options_monitored') }}
-                                      join {{ ref('base_tickers') }} using (symbol)
-                                      where exchange_canonical is not null
-                                  ),
-                                  time_series_1d as
-                                       (
-                                           SELECT distinct exchange_canonical, date
-                                           FROM {{ ref('historical_prices') }}
-                                                    join filtered_base_tickers on filtered_base_tickers.symbol = historical_prices.code
-                                           where date >= now() - interval '1 year' - interval '1 week'
-                                       )
-                             select symbol,
-                                    time_series_1d.date,
-                                    null::double precision as open,
-                                    null::double precision as high,
-                                    null::double precision as low,
-                                    null::double precision as close,
-                                    null::double precision as adjusted_close,
-                                    null::double precision as volume,
-                                    1                      as priority
-                             from filtered_base_tickers
-                                      join time_series_1d using (exchange_canonical)
-                         )
-                         union all
-                         (
-                             with filtered_base_tickers as
-                                       (
-                                          select symbol, country_name
-                                          from {{ ref('base_tickers') }}
-                                          where exchange_canonical is null
-                                            and (country_name in ('USA') or country_name is null)
-                                          union all
-                                          select contract_name as symbol, country_name
-                                          from {{ ref('ticker_options_monitored') }}
-                                          join {{ ref('base_tickers') }} using (symbol)
-                                          where exchange_canonical is null
-                                            and (country_name in ('USA') or country_name is null)
-                                       ),
-                                  time_series_1d as
-                                       (
-                                           SELECT distinct country_name, date
-                                            FROM {{ ref('historical_prices') }}
-                                                     join filtered_base_tickers on filtered_base_tickers.symbol = historical_prices.code
-                                               where date >= now() - interval '1 year' - interval '1 week'
-                                       )
-                             select symbol,
-                                    time_series_1d.date,
-                                    null::double precision as open,
-                                    null::double precision as high,
-                                    null::double precision as low,
-                                    null::double precision as close,
-                                    null::double precision as adjusted_close,
-                                    null::double precision as volume,
-                                    1                      as priority
-                             from filtered_base_tickers
-                                      join time_series_1d using (country_name)
-                          )
-                          union all
-                          (
-                              with filtered_base_tickers as
-                                       (
-                                           select symbol, country_name
-                                           from {{ ref('base_tickers') }}
-                                           where type = 'crypto'
-                                       ),
-                                   time_series_1d as
-                                       (
-                                           SELECT distinct date
-                                           FROM {{ ref('historical_prices') }}
-                                               join filtered_base_tickers on filtered_base_tickers.symbol = historical_prices.code
-                                           where date >= now() - interval '1 year' - interval '1 week'
-                                       )
-                              select symbol,
-                                     time_series_1d.date,
-                                     null::double precision as open,
-                                     null::double precision as high,
-                                     null::double precision as low,
-                                     null::double precision as close,
-                                     null::double precision as adjusted_close,
-                                     null::double precision as volume,
-                                     1                      as priority
-                              from filtered_base_tickers
-                                       join time_series_1d on true
-                          )
-                     ) t
-                 left join {{ ref('ticker_options_monitored') }} on ticker_options_monitored.contract_name = t.symbol
-                 join {{ ref('base_tickers') }} on base_tickers.symbol = ticker_options_monitored.symbol or base_tickers.symbol = t.symbol
-                 left join {{ ref('exchange_holidays') }}
-                           on (exchange_holidays.exchange_name = base_tickers.exchange_canonical or
-                               (base_tickers.exchange_canonical is null and exchange_holidays.country_name = base_tickers.country_name))
-                               and exchange_holidays.date = t.date
-                 where exchange_holidays.date is null
-                 order by t.symbol, t.date, priority
+                 select code as symbol, min(date) as min_date
+                 from {{ ref('historical_prices') }}
+                 group by code
+             ),
+         tickers_dates_skeleton as
+             (
+                 (
+                     -- exchange tickers
+                     with filtered_base_tickers as
+                              (
+                                  select symbol, exchange_canonical
+                                  from {{ ref('base_tickers') }}
+                                  where exchange_canonical is not null
+                                  union all
+                                  select contract_name as symbol, exchange_canonical
+                                  from {{ ref('ticker_options_monitored') }}
+                                           join {{ ref('base_tickers') }} using (symbol)
+                                  where exchange_canonical is not null
+                              )
+                     SELECT symbol as code, date_series::date as date
+                     FROM generate_series(now() - interval '1 year' - interval '1 week', now(), '1 day') date_series
+                              join filtered_base_tickers on true
+                              join uniq_tickers using (symbol)
+                              left join {{ ref('exchange_holidays') }}
+                                        on exchange_holidays.exchange_name = filtered_base_tickers.exchange_canonical
+                                            and exchange_holidays.date = date_series::date
+                     where exchange_holidays.date is null
+                       and extract(isodow from date_series) < 6
+                       and date_series::date >= min_date
+                 )
+                 union all
+                 (
+                     -- USA tickers without exchange
+                     with filtered_base_tickers as
+                              (
+                                  select symbol, country_name
+                                  from {{ ref('base_tickers') }}
+                                  where exchange_canonical is null
+                                    and (country_name in ('USA') or country_name is null)
+                                  union all
+                                  select contract_name as symbol, country_name
+                                  from {{ ref('ticker_options_monitored') }}
+                                           join {{ ref('base_tickers') }} using (symbol)
+                                  where exchange_canonical is null
+                                    and (country_name in ('USA') or country_name is null)
+                              )
+                     SELECT symbol as code, date_series::date as date
+                     FROM generate_series(now() - interval '1 year' - interval '1 week', now(), '1 day') date_series
+                              join filtered_base_tickers on true
+                              join uniq_tickers using (symbol)
+                              left join {{ ref('exchange_holidays') }}
+                                        on exchange_holidays.country_name = filtered_base_tickers.country_name
+                                            and exchange_holidays.date = date_series::date
+                     where exchange_holidays.date is null
+                       and extract(isodow from date_series) < 6
+                       and date_series::date >= min_date
+                 )
+                 union all
+                 (
+                     -- crypto
+                     with filtered_base_tickers as
+                              (
+                                  select symbol, country_name
+                                  from {{ ref('base_tickers') }}
+                                  where type = 'crypto'
+                              )
+                     SELECT symbol as code, date_series::date as date
+                     FROM generate_series(now() - interval '1 year' - interval '1 week', now(), '1 day') date_series
+                              join filtered_base_tickers on true
+                              join uniq_tickers using (symbol)
+                     where date_series::date >= min_date
+                 )
              )
-    select *
-    from (
-             select DISTINCT ON (
-                 symbol,
-                 date
-                  ) (symbol || '_' || date || '_1d')::varchar as id,
-                    symbol,
-                    date::timestamp                           as time,
-                    date::timestamp                           as datetime,
-                    '1d'::varchar                             as period,
-                    coalesce(
-                            open,
-                            first_value(close)
-                            OVER (partition by symbol, grp order by date),
-                            historical_prices_marked.price_1y
-                        )                                     as open,
-                    coalesce(
-                            high,
-                            first_value(close)
-                            OVER (partition by symbol, grp order by date),
-                            historical_prices_marked.price_1y
-                        )                                     as high,
-                    coalesce(
-                            low,
-                            first_value(close)
-                            OVER (partition by symbol, grp order by date),
-                            historical_prices_marked.price_1y
-                        )                                     as low,
-                    coalesce(
-                            close,
-                            first_value(close)
-                            OVER (partition by symbol, grp order by date),
-                            historical_prices_marked.price_1y
-                        )                                     as close,
-                    coalesce(
-                            adjusted_close,
-                            first_value(adjusted_close)
-                            OVER (partition by symbol, grp order by date),
-                            historical_prices_marked.price_1y
-                        )                                     as adjusted_close,
-                    coalesce(volume, 0.0)                     as volume
-             from (
-                      select combined_daily_prices.*,
-                             sum(case when close is not null then 1 end)
-                             over (partition by combined_daily_prices.symbol order by date) as grp
-                      from combined_daily_prices
-                  ) t
-                      left join {{ ref('historical_prices_marked') }} using (symbol)
-             order by symbol, date
-         ) t2
-    where t2.close is not null
+    select (tds.code || '_' || tds.date || '_1d')::varchar as id,
+           tds.code                                        as symbol,
+           tds.date::timestamp                             as time,
+           tds.date::timestamp                             as datetime,
+           '1d'::varchar                                   as period,
+           hp.open,
+           hp.high,
+           hp.low,
+           hp.close,
+           coalesce(hp.adjusted_close,
+                    linear_interpolate(
+                            extract(epoch from tds."date"),
+                            LAST_VALUE_IGNORENULLS(extract(epoch from hp."date")) over (lookback),
+                            LAST_VALUE_IGNORENULLS(hp.adjusted_close) over (lookback),
+                            LAST_VALUE_IGNORENULLS(extract(epoch from hp."date")) over (lookforward),
+                            LAST_VALUE_IGNORENULLS(hp.adjusted_close) over (lookforward)
+                        ),
+                    LAST_VALUE_IGNORENULLS(hp.adjusted_close) over (lookforward),
+                    LAST_VALUE_IGNORENULLS(hp.adjusted_close) over (lookback),
+                    0)                                     as adjusted_close,
+           coalesce(volume, 0)                             as volume
+    from tickers_dates_skeleton tds
+             left join {{ ref('historical_prices') }} hp using (code, "date")
+        window
+            lookback as (partition by tds.code order by tds."date" asc),
+            lookforward as (partition by tds.code order by tds."date" desc)
 )
 
 union all
