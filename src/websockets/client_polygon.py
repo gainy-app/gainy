91cd0a6be1423d397b50e66a00c79372df921f25
Mikhail Astashkevich <asmisha@tut.by>
2022-07-05 18:13:19 +0200

    Reconnect individual realtime sublisteners

--- src/websockets/client_polygon.py
+++ src/websockets/client_polygon.py
@@ -199,12 +199,6 @@ class PricesListener(AbstractPriceListener):
             self.logger.exception(e)
 
     def should_reconnect(self):
-        if self.sub_listeners is not None:
-            for sub_listener in self.sub_listeners:
-                if not sub_listener.should_reconnect():
-                    return False
-            return True
-
         return super().should_reconnect(self.cluster)
 
     def transform_symbol(self, symbol):
