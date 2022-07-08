c75a348fe9bce4a34f5fd72ecee84221d5dc64b7
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-07-08 12:48:04 +0200

    Optimize portfolio chart (#508)
    * Optimize portfolio chart
    
    * fix

--- src/meltano/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
+++ src/meltano/meltano/transform/models/portfolio/portfolio_transaction_chart.sql
@@ -12,7 +12,8 @@
 }}
 
 
--- Execution Time: 108162.002 ms
+-- Execution Time: 39525.849 ms
+-- Execution Time: 47097.705 ms realtime
 with first_profile_transaction_date as
          (
              select profile_id,
@@ -31,32 +32,31 @@ with first_profile_transaction_date as
 {% endif %}
          )
 
-select (portfolio_expanded_transactions.uniq_id || '_' || chart.period || '_' || chart.datetime) as id,
-       portfolio_expanded_transactions.uniq_id                                                   as transactions_uniq_id,
-       chart.datetime,
-       chart.period,
-       portfolio_expanded_transactions.quantity_norm_for_valuation * chart.open                  as open,
-       portfolio_expanded_transactions.quantity_norm_for_valuation * chart.high                  as high,
-       portfolio_expanded_transactions.quantity_norm_for_valuation * chart.low                   as low,
-       portfolio_expanded_transactions.quantity_norm_for_valuation * chart.close                 as close,
-       portfolio_expanded_transactions.quantity_norm_for_valuation * chart.adjusted_close        as adjusted_close,
-       portfolio_expanded_transactions.updated_at                                                as transactions_updated_at
-from {{ ref('portfolio_expanded_transactions') }}
-         left join first_profile_transaction_date
-                   on first_profile_transaction_date.profile_id = portfolio_expanded_transactions.profile_id
-         join {{ ref('portfolio_securities_normalized') }}
-              on portfolio_securities_normalized.id = portfolio_expanded_transactions.security_id
+select *
+from (
+         select (portfolio_expanded_transactions.uniq_id || '_' || chart.period || '_' || chart.datetime) as id,
+                portfolio_expanded_transactions.uniq_id                                                   as transactions_uniq_id,
+                chart.datetime,
+                chart.period,
+                portfolio_expanded_transactions.quantity_norm_for_valuation * chart.open                  as open,
+                portfolio_expanded_transactions.quantity_norm_for_valuation * chart.high                  as high,
+                portfolio_expanded_transactions.quantity_norm_for_valuation * chart.low                   as low,
+                portfolio_expanded_transactions.quantity_norm_for_valuation * chart.close                 as close,
+                portfolio_expanded_transactions.quantity_norm_for_valuation * chart.adjusted_close        as adjusted_close,
+                portfolio_expanded_transactions.updated_at                                                as transactions_updated_at
+         from {{ ref('portfolio_expanded_transactions') }}
+                  left join first_profile_transaction_date using (profile_id)
+                  join {{ ref('portfolio_securities_normalized') }}
+                       on portfolio_securities_normalized.id = portfolio_expanded_transactions.security_id
+                  join {{ ref('chart') }}
+                       on chart.symbol = portfolio_securities_normalized.original_ticker_symbol
+                           and (chart.datetime >= portfolio_expanded_transactions.date or portfolio_expanded_transactions.date is null)
+                           and (chart.datetime >= first_profile_transaction_date.datetime or first_profile_transaction_date.profile_id is null)
+     ) t
+
 {% if is_incremental() %}
-         left join old_model_stats
-                   on old_model_stats.transactions_uniq_id = portfolio_expanded_transactions.uniq_id
-{% endif %}
-         join {{ ref('chart') }}
-              on chart.symbol = portfolio_securities_normalized.original_ticker_symbol
-                  and (chart.datetime >= portfolio_expanded_transactions.date or portfolio_expanded_transactions.date is null)
-                  and (chart.datetime >= first_profile_transaction_date.datetime or first_profile_transaction_date.profile_id is null)
-{% if is_incremental() %}
-                  and (old_model_stats.transactions_uniq_id is null
-                      or (chart.period = old_model_stats.period
-                          and (portfolio_expanded_transactions.updated_at > old_model_stats.max_transactions_updated_at -- new / updated transaction - recalc all
-                              or chart.datetime > old_model_stats.max_datetime))) -- old transaction - recalc only new
+         left join old_model_stats using (transactions_uniq_id, period)
+where old_model_stats.transactions_uniq_id is null
+   or t.transactions_updated_at > old_model_stats.max_transactions_updated_at -- new / updated transaction - recalc all
+   or t.datetime > old_model_stats.max_datetime -- old transaction - recalc only new
 {% endif %}
