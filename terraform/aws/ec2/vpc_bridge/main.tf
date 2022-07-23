57435b2503214fc7b4787c002e6d868944197524
Mikhail Astashkevich <asmisha@tut.by>
2022-07-23 07:32:42 +0200

    fix tf

--- terraform/aws/ec2/vpc_bridge/main.tf
+++ terraform/aws/ec2/vpc_bridge/main.tf
@@ -104,7 +104,7 @@ resource "random_password" "internal_sync_postgres" {
 resource "aws_instance" "bridge" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
-  key_name                    = module.key_pair.key_pair_key_name
+  key_name                    = module.key_pair.key_pair_name
   subnet_id                   = var.public_subnet_id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.bridge.id]
