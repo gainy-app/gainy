fe1d4c00ee08c1b88ca365d71db60507f80b5d62
Mikhail Astashkevich <asmisha@tut.by>
2022-08-03 14:50:31 +0200

    fix

--- src/meltano/meltano/transform/models/ticker_components.sql
+++ src/meltano/meltano/transform/models/ticker_components.sql
@@ -39,29 +39,46 @@ with
                     version,
                     updated_at
              from versioned_data
+         ),
+     data as
+         (
+             select expanded.code                                  as symbol,
+                    expanded.key                                   as original_component_symbol,
+                    expanded.value ->> 'Code'                      as component_symbol,
+                    expanded.value ->> 'Name'                      as component_name,
+                    expanded.value ->> 'Region'                    as component_region,
+                    expanded.value ->> 'Sector'                    as component_sector,
+                    expanded.value ->> 'Country'                   as component_country,
+                    (expanded.value ->> 'Assets_%')::numeric / 100 as component_weight,
+                    expanded.value ->> 'Exchange'                  as component_exchange,
+                    expanded.value ->> 'Industry'                  as component_industry,
+                    expanded.version,
+                    expanded.updated_at
+             from expanded
+                 join {{ ref('tickers') }} on tickers.symbol = (expanded.value ->> 'Code')
+             {% if is_incremental() %}
+                 left join old_version using (code)
+             {% endif %}
+
+             where tickers.name = (expanded.value ->> 'Name')
+
+             {% if is_incremental() %}
+               and expanded.version != old_version.version or old_version is null
+             {% endif %}
          )
-select expanded.code                                           as symbol,
-       expanded.key                                            as original_component_symbol,
-       expanded.value ->> 'Code'                               as component_symbol,
-       expanded.value ->> 'Name'                               as component_name,
-       expanded.value ->> 'Region'                             as component_region,
-       expanded.value ->> 'Sector'                             as component_sector,
-       expanded.value ->> 'Country'                            as component_country,
-       (expanded.value ->> 'Assets_%')::numeric / 100          as component_weight,
-       (expanded.value ->> 'Assets_%')::double precision / 100 as component_weight_float,
-       expanded.value ->> 'Exchange'                           as component_exchange,
-       expanded.value ->> 'Industry'                           as component_industry,
-       expanded.code || '_' || (expanded.value ->> 'Code')     as id,
-       expanded.version,
-       expanded.updated_at
-from expanded
-    join {{ ref('tickers') }} on tickers.symbol = (expanded.value ->> 'Code')
-{% if is_incremental() %}
-    left join old_version using (code)
-{% endif %}
-
-where tickers.name  = (expanded.value ->> 'Name')
-
-{% if is_incremental() %}
-  and expanded.version != old_version.version or old_version is null
-{% endif %}
+select symbol,
+       min(original_component_symbol)          as original_component_symbol,
+       component_symbol,
+       min(component_name)                     as component_name,
+       min(component_region)                   as component_region,
+       min(component_sector)                   as component_sector,
+       min(component_country)                  as component_country,
+       sum(component_weight)                   as component_weight,
+       sum(component_weight)::double precision as component_weight_float,
+       min(component_exchange)                 as component_exchange,
+       min(component_industry)                 as component_industry,
+       symbol || '_' || component_symbol       as id,
+       min(version)                            as version,
+       min(updated_at)                         as updated_at
+from data
+group by symbol, component_symbol
