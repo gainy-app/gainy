5f9d262cbe3896be53bdc2095a0c8ede2d92ef19
Mikhail Astashkevich <asmisha@tut.by>
2022-08-03 09:39:01 +0200

    fix resources and cleanup script

--- terraform/aws/ecs/services/locals_resources.tf
+++ terraform/aws/ecs/services/locals_resources.tf
@@ -4,7 +4,7 @@ locals {
   polygon_websockets_cpu_credits = var.env == "production" ? 128 : 0
   hasura_cpu_credits             = var.env == "production" ? 1024 : 256
   meltano_ui_cpu_credits         = var.env == "production" ? 256 : 128
-  meltano_scheduler_cpu_credits  = var.env == "production" ? 2048 : 256
+  meltano_scheduler_cpu_credits  = var.env == "production" ? 2048 : 512
   main_cpu_credits = ceil(sum([
     local.eod_websockets_cpu_credits,
     local.polygon_websockets_cpu_credits,
@@ -13,18 +13,18 @@ locals {
     local.meltano_scheduler_cpu_credits,
   ]) / 1024) * 1024
 
-  upstream_pool_size                  = 3
+  upstream_pool_size                  = 4
   eodhistoricaldata_jobs_count        = 4
   eodhistoricaldata_prices_jobs_count = 2
   coingecko_jobs_count                = 2
-  polygon_jobs_count                  = 12
+  polygon_jobs_count                  = 2
 
   eod_symbols_limit                 = var.env == "production" ? 14000 : 20
   eod_websockets_memory_credits     = var.env == "production" ? 512 : 256
   polygon_websockets_memory_credits = var.env == "production" ? 1024 : 0
   hasura_memory_credits             = var.env == "production" ? 2048 : 1024
   meltano_ui_memory_credits         = var.env == "production" ? 1024 : 1024
-  meltano_scheduler_memory_credits  = var.env == "production" ? 5120 : 3072
+  meltano_scheduler_memory_credits  = var.env == "production" ? 3584 : 2816
   main_memory_credits = ceil(sum([
     local.hasura_memory_credits,
     local.meltano_ui_memory_credits,
