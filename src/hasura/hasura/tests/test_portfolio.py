0223c0acf8720ff92e29ac030e6f404697a3ed7c
Mikhail Astashkevich <asmisha@tut.by>
2022-08-16 16:52:14 +0200

    add test

--- src/hasura/hasura/tests/test_portfolio.py
+++ src/hasura/hasura/tests/test_portfolio.py
@@ -25,6 +25,21 @@ def test_portfolio():
         data['profile_holding_groups']) >= MIN_PORTFOLIO_HOLDING_GROUPS_COUNT
 
 
+def test_demo_portfolio():
+    query_file = os.path.join(os.path.dirname(__file__),
+                              'queries/GetPortfolioHoldings.graphql')
+    with open(query_file, 'r') as f:
+        query = f.read()
+
+    profile_ids = {profile['id']: profile['user_id'] for profile in PROFILES}
+    data = make_graphql_request(query, {"profileId": PROFILE_ID},
+                                profile_ids[2])['data']
+    assert data['portfolio_gains'] is not None
+    assert data['profile_holding_groups'] is not None
+    assert len(
+        data['profile_holding_groups']) >= MIN_PORTFOLIO_HOLDING_GROUPS_COUNT
+
+
 def get_test_portfolio_chart_filters_data():
     full_options_dict = {
         "periods": ["1d"],
