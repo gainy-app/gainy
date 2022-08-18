c661238e2aad29e82cfc33d3b08e76abb4872f90
Mikhail Astashkevich <asmisha@tut.by>
2022-08-18 08:02:30 +0200

    Collection Recommendations Fallback

--- src/aws/lambda-python/lambda_python/recommendation/recommendation_action.py
+++ src/aws/lambda-python/lambda_python/recommendation/recommendation_action.py
@@ -1,7 +1,8 @@
 from common.hasura_function import HasuraAction
+from gainy.data_access.db_lock import LockAcquisitionTimeout
 from gainy.data_access.optimistic_lock import ConcurrentVersionUpdate
 from gainy.recommendation.compute import ComputeRecommendationsAndPersist
-from gainy.recommendation.repository import RecommendationRepository
+from gainy.recommendation.repository import RecommendationRepository, RecommendedCollectionAlgorithm
 from gainy.utils import get_logger
 
 logger = get_logger(__name__)
@@ -16,23 +17,23 @@ class GetRecommendedCollections(HasuraAction):
         try:
             profile_id = input_params["profile_id"]
             limit = input_params.get("limit", 30)
-            force = input_params.get("force", False)
             logging_extra = {
                 'profile_id': profile_id,
-                'force': force,
             }
-
-            if force:
-                self.update_match_scores(db_conn, profile_id)
 
             repository = RecommendationRepository(db_conn)
             collections = repository.get_recommended_collections(
                 profile_id, limit)
 
-            if not len(collections) and not force:
+            if not len(collections):
                 self.update_match_scores(db_conn, profile_id)
                 collections = repository.get_recommended_collections(
                     profile_id, limit)
+
+            if not len(collections):
+                collections = repository.get_recommended_collections(
+                    profile_id, limit,
+                    RecommendedCollectionAlgorithm.TOP_FAVORITED)
 
             if not len(collections):
                 logger.error(
@@ -54,5 +55,5 @@ class GetRecommendedCollections(HasuraAction):
             db_conn, profile_id)
         try:
             recommendations_func.get_and_persist(db_conn, max_tries=2)
-        except ConcurrentVersionUpdate:
+        except (LockAcquisitionTimeout, ConcurrentVersionUpdate):
             pass
