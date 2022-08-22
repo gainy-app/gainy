d65485aad5272a5c4d2443e058094295930a6023
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-08-22 16:52:55 +0200

    Store Collection Match scores (#569)
    * Store Collection Match scores
    
    * fixes
    
    * update version

---
+++ src/hasura/hasura/migrations/1661163247230_create_table_app_profile_collection_match_score/up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "app"."profile_collection_match_score"
+(
+    "profile_id"          integer   NOT NULL,
+    "collection_id"       integer   NOT NULL,
+    "collection_uniq_id"  text      NOT NULL,
+    "match_score"         float8    NOT NULL,
+    "risk_similarity"     float8    NOT NULL,
+    "category_similarity" float8    NOT NULL,
+    "interest_similarity" float8    NOT NULL,
+    "updated_at"          timestamp NOT NULL,
+    "risk_level"          integer   NOT NULL,
+    "category_level"      integer   NOT NULL,
+    "interest_level"      integer   NOT NULL,
+    PRIMARY KEY ("profile_id", "collection_uniq_id"),
+    FOREIGN KEY ("profile_id") REFERENCES "app"."profiles" ("id") ON UPDATE cascade ON DELETE cascade
+);
--- src/meltano/meltano/transform/models/collections/collection_match_score.sql
+++ src/meltano/meltano/transform/models/collections/collection_match_score.sql
@@ -5,25 +5,5 @@
   )
 }}
 
-select t.*,
-       (risk_similarity > 0.3)::int + (risk_similarity > 0.7)::int         as risk_level,
-       (category_similarity > 0.3)::int + (category_similarity > 0.7)::int as category_level,
-       (interest_similarity > 0.3)::int + (interest_similarity > 0.7)::int as interest_level
-from (
-         select profile_ticker_match_score.profile_id,
-                user_id,
-                collection_id,
-                collection_uniq_id,
-                (sum(match_score * weight) / sum(weight))::double precision         as match_score,
-                (sum(risk_similarity * weight) / sum(weight))::double precision     as risk_similarity,
-                (sum(category_similarity * weight) / sum(weight))::double precision as category_similarity,
-                (sum(interest_similarity * weight) / sum(weight))::double precision as interest_similarity
-         from {{ source('app', 'profiles') }}
-                  join {{ ref('collection_tickers_weighted') }}
-                       on (collection_tickers_weighted.profile_id is null or
-                           collection_tickers_weighted.profile_id = profiles.id)
-                  join {{ source('app', 'profile_ticker_match_score') }}
-                       on profile_ticker_match_score.profile_id = profiles.id
-                           and profile_ticker_match_score.symbol = collection_tickers_weighted.symbol
-         group by user_id, collection_uniq_id, profile_ticker_match_score.profile_id, collection_id
-     ) t
+select *
+from {{ source('app', 'profile_collection_match_score') }}
