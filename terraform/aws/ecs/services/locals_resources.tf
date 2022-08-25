750fd772a3a3b0f565a8663c410fa8df6529c235
Mikhail Astashkevich <asmisha@tut.by>
2022-08-25 15:52:13 +0200

    ETF tags ranks

--- terraform/aws/ecs/services/locals_resources.tf
+++ terraform/aws/ecs/services/locals_resources.tf
@@ -13,9 +13,9 @@ locals {
     local.meltano_scheduler_cpu_credits,
   ]) / 1024) * 1024
 
-  upstream_pool_size                  = var.env == "production" ? 3 : 2
+  upstream_pool_size                  = var.env == "production" ? 3 : 1
   eodhistoricaldata_jobs_count        = 3
-  eodhistoricaldata_prices_jobs_count = 2
+  eodhistoricaldata_prices_jobs_count = 4
   coingecko_jobs_count                = 1
   polygon_jobs_count                  = 6
 
