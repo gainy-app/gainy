77f19d0e933e138b279ee82598eb081c25ea13cb
Mikhail Astashkevich <asmisha@tut.by>
2022-09-07 07:41:57 +0300

    fix

--- src/aws/lambda-python/lambda_python/trading/actions/__init__.py
+++ src/aws/lambda-python/lambda_python/trading/actions/__init__.py
@@ -7,4 +7,4 @@ from trading.actions.get_funding_accounts import TradingGetFundingAccounts
 from trading.actions.delete_funding_account import TradingDeleteFundingAccount
 from trading.actions.deposit_funds import TradingDepositFunds
 from trading.actions.withdraw_funds import TradingWithdrawFunds
-from trading.actions.reconfigure_collection_holdings.py import TradingReconfigureCollectionHoldings
+from trading.actions.reconfigure_collection_holdings import TradingReconfigureCollectionHoldings
