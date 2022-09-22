217286f578dd9fd409ec29bf7bce57714fc0decc
Mikhail Astashkevich <asmisha@tut.by>
2022-09-22 16:08:06 +0200

    fix tf

--- terraform/aws/lambda/main.tf
+++ terraform/aws/lambda/main.tf
@@ -306,7 +306,7 @@ module "sqs_listener_integration" {
   aws_iam_role_lambda_exec_role = aws_iam_role.lambda_exec
   aws_lambda_invoke_arn         = "${module.sqs_listener.arn}:${module.sqs_listener.version}"
 
-  sqs_batch_size = 100
+  sqs_batch_size = 10
   sqs_queue_arns = var.drivewealth_sqs_arn != "" ? [var.drivewealth_sqs_arn] : []
 }
 
