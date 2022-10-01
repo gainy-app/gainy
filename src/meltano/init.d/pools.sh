879ba81689fec48a9cdbf9f6272d14c4f66364df
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-10-01 10:19:49 +0200

    Decrease downstream sync volume (#607)
    * Decrease downstream sync volume
    
    * fix

--- src/meltano/init.d/pools.sh
+++ src/meltano/init.d/pools.sh
@@ -1,3 +1,4 @@
 meltano invoke airflow pools set dbt 1 dbt
 meltano invoke airflow pools set upstream $UPSTREAM_POOL_SIZE upstream
+meltano invoke airflow pools set downstream $DOWNSTREAM_POOL_SIZE downstream
 meltano invoke airflow pools set gainy_recommendation 1 gainy_recommendation
