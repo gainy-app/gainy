792d789a7ae5c2b917e9173d15615c965aa0fe75
Mikhail Astashkevich <asmisha@tut.by>
2022-10-06 17:42:47 +0200

    Fix api error handling

--- src/aws/lambda-python/lambda_python/trading/drivewealth/event_handlers/instrument_updated.py
+++ src/aws/lambda-python/lambda_python/trading/drivewealth/event_handlers/instrument_updated.py
@@ -1,3 +1,4 @@
+from gainy.trading.drivewealth.exceptions import InstrumentNotFoundException
 from gainy.utils import get_logger
 from trading.drivewealth.abstract_event_handler import AbstractDriveWealthEventHandler
 
@@ -10,5 +11,8 @@ class InstrumentUpdatedEventHandler(AbstractDriveWealthEventHandler):
         return event_type == 'instruments.updated'
 
     def handle(self, event_payload: dict):
-        ref_id = event_payload["instrumentID"]
-        self.provider.sync_instrument(ref_id=ref_id)
+        try:
+            ref_id = event_payload["instrumentID"]
+            self.provider.sync_instrument(ref_id=ref_id)
+        except InstrumentNotFoundException:
+            pass
