9a68ee31717906d6cb1da035cd9d5eb26baf5dd8
Mikhail Astashkevich <asmisha@tut.by>
2022-10-31 17:58:35 +0100

    fix local meltano seeds

--- src/meltano/meltano/seed/local/01_raw_data_eod_historical_prices.sql
+++ src/meltano/meltano/seed/local/01_raw_data_eod_historical_prices.sql
@@ -4,18 +4,32 @@ with latest_historical_prices as
              select distinct on (code) *
              from raw_data.eod_historical_prices
              order by code, date desc
-         )
-select latest_historical_prices.code as code,
-       latest_historical_prices.adjusted_close + random() * 10 - 5 as adjusted_close,
-       latest_historical_prices.close + random() * 10 - 5 as close,
-       dd::date as date,
-       latest_historical_prices.high + random() * 10 - 5 as high,
-       latest_historical_prices.low + random() * 10 - 5 as low,
-       latest_historical_prices.open + random() * 10 - 5 as open,
-       latest_historical_prices.volume * (random() + 0.5) as volume
-FROM generate_series((select min(date) from latest_historical_prices)::date + interval '1 day', now() - interval '1 day', interval '1 day') dd
-         join latest_historical_prices on true
-where extract(isodow from dd) < 6
+         ),
+    with_random as
+        (
+              select latest_historical_prices.code,
+                     random() as r1,
+                     random() as r2,
+                     latest_historical_prices.adjusted_close,
+                     latest_historical_prices.close,
+                     dd::date as date,
+                     latest_historical_prices.high,
+                     latest_historical_prices.low,
+                     latest_historical_prices.open,
+                     latest_historical_prices.volume
+              FROM generate_series((select min(date) from latest_historical_prices)::date + interval '1 day', now() - interval '1 day', interval '1 day') dd
+                       join latest_historical_prices on true
+              where extract(isodow from dd) < 6
+     )
+select code,
+       adjusted_close * (r1 / 10 + 1) as adjusted_close,
+       close * (r1 / 10 + 1) as close,
+       date,
+       high * (r1 / 10 + 1) as high,
+       low * (r1 / 10 + 1) as low,
+       open * (r1 / 10 + 1) as open,
+       volume * (r2 + 0.5) as volume
+FROM with_random
 on conflict do nothing;
 
 with min_dates as
--- src/meltano/meltano/seed/local/01_raw_data_polygon_crypto_historical_prices.sql
+++ src/meltano/meltano/seed/local/01_raw_data_polygon_crypto_historical_prices.sql
@@ -4,17 +4,35 @@ with latest_historical_prices as
              select distinct on (symbol) symbol, t, o, h, l, c, v
              from raw_data.polygon_crypto_historical_prices
              order by symbol, t desc
-         )
-select latest_historical_prices.symbol,
-       dd                                             as t,
-       latest_historical_prices.o + random() * 10 - 5 as o,
-       latest_historical_prices.h + random() * 10 - 5 as h,
-       latest_historical_prices.l + random() * 10 - 5 as l,
-       latest_historical_prices.c + random() * 10 - 5 as c,
-       latest_historical_prices.v * (random() + 0.5)  as v
-FROM generate_series((
-                         select min(t)
-                         from latest_historical_prices
-                     ) + 86400 * 1000, extract(epoch from now() - interval '1 day')::numeric * 1000, 86400 * 1000) dd
-         join latest_historical_prices on true
-where extract(isodow from to_timestamp(dd / 1000)) < 6;
+         ),
+     with_random as
+         (
+             select latest_historical_prices.symbol,
+                    random() as r1,
+                    random() as r2,
+                    dd       as t,
+                    latest_historical_prices.o,
+                    latest_historical_prices.h,
+                    latest_historical_prices.l,
+                    latest_historical_prices.c,
+                    latest_historical_prices.v
+             FROM generate_series((
+                                      select min(t)
+                                      from latest_historical_prices
+                                  ) + 86400 * 1000, extract(epoch from now() - interval '1 day')::numeric * 1000,
+                                  86400 * 1000) dd
+                      join latest_historical_prices on true
+                      join (
+                               select random() as r
+                           ) r on true
+             where extract(isodow from to_timestamp(dd / 1000)) < 6
+     )
+select symbol,
+       t,
+       o * (r1 / 10 + 1),
+       h * (r1 / 10 + 1),
+       l * (r1 / 10 + 1),
+       c * (r1 / 10 + 1),
+       v * (r2 + 0.5)
+FROM with_random
+on conflict do nothing;
