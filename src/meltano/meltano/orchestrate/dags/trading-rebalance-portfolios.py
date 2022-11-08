08b22781b3523025f7753e0e82ca923114390c66
Mikhail Astashkevich <asmisha@tut.by>
2022-11-08 12:52:36 +0100

    Fetch all drivewealth instruments

--- src/meltano/meltano/orchestrate/dags/trading-rebalance-portfolios.py
+++ src/meltano/meltano/orchestrate/dags/trading-rebalance-portfolios.py
@@ -2,7 +2,7 @@ from airflow.operators.bash import BashOperator
 from common import create_dag
 
 dag_id = "trading-rebalance-portfolios"
-tags = ["trading"]
+tags = ["trading", "drivewealth"]
 dag = create_dag(dag_id,
                  tags=tags,
                  is_paused_upon_creation=True,
