ab5772531f2a34d5eb81bace1044666d5ff54737
Mikhail Astashkevich <asmisha@tut.by>
2022-11-23 08:41:39 +0100

    fix

--- src/meltano/meltano/transform/models/eod/earnings_history.sql
+++ src/meltano/meltano/transform/models/eod/earnings_history.sql
@@ -1,7 +1,6 @@
 {{
   config(
     materialized = "table",
-    dist = "symbol",
     post_hook=[
       pk('symbol, date'),
     ]
@@ -9,25 +8,25 @@
 }}
 
 
-    with expanded as (
-        select code    as symbol,
-               (json_each((earnings -> 'History')::json)).*,
-               case
-                   when is_date(updatedat)
-                       then updatedat::timestamp
-                   else _sdc_batched_at
-                   end as updated_at
-        from {{ source('eod', 'eod_fundamentals') }}
-    )
-    select symbol,
-           key::date                              as date,
-           (value ->> 'currency')::text           as currency,
-           (value ->> 'epsActual')::numeric       as eps_actual,
-           (value ->> 'reportDate')::date         as report_date,
-           (value ->> 'epsEstimate')::numeric     as eps_estimate,
-           (value ->> 'epsDifference')::numeric   as eps_difference,
-           (value ->> 'surprisePercent')::numeric as surprise_percent,
-           (value ->> 'beforeAfterMarket')::text  as before_after_market,
-           updated_at
-    from expanded
-    where key != '0000-00-00'
+with expanded as (
+    select code    as symbol,
+           (json_each((earnings -> 'History')::json)).*,
+           case
+               when is_date(updatedat)
+                   then updatedat::timestamp
+               else _sdc_batched_at
+               end as updated_at
+    from {{ source('eod', 'eod_fundamentals') }}
+)
+select expanded.symbol,
+       expanded.key::date                              as date,
+       (expanded.value ->> 'currency')::text           as currency,
+       (expanded.value ->> 'epsActual')::numeric       as eps_actual,
+       (expanded.value ->> 'reportDate')::date         as report_date,
+       (expanded.value ->> 'epsEstimate')::numeric     as eps_estimate,
+       (expanded.value ->> 'epsDifference')::numeric   as eps_difference,
+       (expanded.value ->> 'surprisePercent')::numeric as surprise_percent,
+       (expanded.value ->> 'beforeAfterMarket')::text  as before_after_market,
+       expanded.updated_at
+from expanded
+where key != '0000-00-00'
