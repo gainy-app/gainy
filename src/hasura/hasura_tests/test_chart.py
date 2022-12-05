e3d7ddcf437c2dbcdbdb3e343784c1832291843a
Mikhail Astashkevich <asmisha@tut.by>
2022-12-05 17:53:43 +0100

    fix tests

--- src/hasura/hasura_tests/test_chart.py
+++ src/hasura/hasura_tests/test_chart.py
@@ -9,8 +9,8 @@ def test_chart():
     query = 'query DiscoverCharts($period: String!, $symbol: String!) { chart(where: {symbol: {_eq: $symbol}, period: {_eq: $period}}, order_by: {datetime: asc}) { symbol datetime period open high low close adjusted_close volume } }'
     datasets = [
         ("1d", 0),
-        ("1w", 50),
-        ("1m", 20),
+        ("1w", 60),
+        ("1m", 15),
     ]
 
     for (period, min_count) in datasets:
