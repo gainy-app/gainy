1614dbcf380a500fc568f3bbd69fa204f7480362
Michael Astashkevich <asmisha@users.noreply.github.com>
2022-12-07 11:03:00 +0100

    Profile flag is_personalization_enabled (#704)
    * Profile flag is_personalization_enabled
    
    * Update schema.yml

--- src/meltano/meltano/transform/models/profile_flags.sql
+++ src/meltano/meltano/transform/models/profile_flags.sql
@@ -6,7 +6,9 @@
 }}
 
 select profiles.id                                               as profile_id,
+       profile_scoring_settings.profile_id is not null           as is_personalization_enabled,
        coalesce(profile_flags.is_region_changing_allowed, false) as is_region_changing_allowed,
        coalesce(profile_flags.is_trading_enabled, false)         as is_trading_enabled
 from {{ source('app', 'profiles') }}
          left join {{ source('app', 'profile_flags') }} on profile_flags.profile_id = profiles.id
+         left join {{ source('app', 'profile_scoring_settings') }} on profile_scoring_settings.profile_id = profiles.id
