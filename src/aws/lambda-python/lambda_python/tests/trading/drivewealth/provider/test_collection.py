27407813e8729ec2c1b02956c80b1b481f2ee740
Mikhail Astashkevich <asmisha@tut.by>
2022-12-08 12:07:13 +0100

    fix tests

--- src/aws/lambda-python/lambda_python/tests/trading/drivewealth/provider/test_collection.py
+++ src/aws/lambda-python/lambda_python/tests/trading/drivewealth/provider/test_collection.py
@@ -75,9 +75,9 @@ def test_get_actual_collection_data(monkeypatch):
         assert _trading_account_id == trading_account_id
         return portfolio
 
-    def mock_get_profile_fund(_profile_id, _collection_id=None):
+    def mock_get_profile_fund(_profile_id, **kwargs):
         assert _profile_id == profile_id
-        assert _collection_id == collection_id
+        assert kwargs["collection_id"] == collection_id
         return fund
 
     drivewealth_repository = DriveWealthRepository(None)
