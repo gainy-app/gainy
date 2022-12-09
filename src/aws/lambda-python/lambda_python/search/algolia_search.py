5cce050a0f76067e2e51893943c8c55148f6cbcc
Mikhail Astashkevich <asmisha@tut.by>
2022-12-09 19:49:53 +0100

    Algolia user token

--- src/aws/lambda-python/lambda_python/search/algolia_search.py
+++ src/aws/lambda-python/lambda_python/search/algolia_search.py
@@ -7,16 +7,20 @@ from gainy.recommendation import TOP_20_FOR_YOU_COLLECTION_ID
 from common.context_container import ContextContainer
 from common.hasura_function import HasuraAction
 from operator import itemgetter
+from gainy.utils import get_logger
+
+logger = get_logger(__name__)
 
 
 class SearchAction(HasuraAction, ABC):
 
     def __init__(self, action_name, algolia_app_id, algolia_search_api_key,
-                 tickers_index, attributes_to_retrieve, key_attribute):
+                 index_name, attributes_to_retrieve, key_attribute):
         super().__init__(action_name)
         search_client = SearchClient.create(algolia_app_id,
                                             algolia_search_api_key)
-        self.search_index = search_client.init_index(tickers_index)
+        self.index_name = index_name
+        self.search_index = search_client.init_index(index_name)
         self.attributes_to_retrieve = attributes_to_retrieve
         self.key_attribute = key_attribute
 
@@ -26,13 +30,19 @@ class SearchAction(HasuraAction, ABC):
         offset = input_params.get("offset", 0)
         limit = input_params.get("limit", 10)
 
-        result = self.search_index.search(
-            query, {
-                "attributesToRetrieve": self.attributes_to_retrieve,
-                "attributesToHighlight": [],
-                "offset": offset,
-                "length": limit
-            })
+        params = {
+            "userToken": self.profile_id,
+            "attributesToRetrieve": self.attributes_to_retrieve,
+            "attributesToHighlight": [],
+            "offset": offset,
+            "length": limit
+        }
+        logger.info('Algolia search',
+                    extra={
+                        "index_name": self.index_name,
+                        **params
+                    })
+        result = self.search_index.search(query, params)
 
         record_list = [
             dict([(attr, hit[attr]) for attr in self.attributes_to_retrieve])
@@ -74,9 +84,9 @@ class SearchAction(HasuraAction, ABC):
 
 class SearchTickers(SearchAction):
 
-    def __init__(self, algolia_app_id, algolia_search_api_key, tickers_index):
+    def __init__(self, algolia_app_id, algolia_search_api_key, index_name):
         super().__init__("search_tickers", algolia_app_id,
-                         algolia_search_api_key, tickers_index, ["symbol"],
+                         algolia_search_api_key, index_name, ["symbol"],
                          "symbol")
 
     def table_name(self) -> str:
@@ -88,9 +98,9 @@ class SearchTickers(SearchAction):
 
 class SearchCollections(SearchAction):
 
-    def __init__(self, algolia_app_id, algolia_search_api_key, tickers_index):
+    def __init__(self, algolia_app_id, algolia_search_api_key, index_name):
         super().__init__("search_collections", algolia_app_id,
-                         algolia_search_api_key, tickers_index, ["id"], "id")
+                         algolia_search_api_key, index_name, ["id"], "id")
 
     def table_name(self) -> str:
         return "collections"
