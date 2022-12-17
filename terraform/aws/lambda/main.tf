4a837cc92ed4c2883c4cd049b0eaf61ed47cb0c2
Mikhail Astashkevich <asmisha@tut.by>
2022-12-17 09:39:43 +0100

    fix

--- terraform/aws/lambda/main.tf
+++ terraform/aws/lambda/main.tf
@@ -275,7 +275,7 @@ module "sqs_listener_integration" {
   aws_lambda_invoke_arn         = "${module.sqs_listener.arn}:${module.sqs_listener.version}"
 
   sqs_batch_size = 10
-  sqs_queue_arns = [for arn in [var.drivewealth_sqs_arn, var.aws_events_sqs_arn] : arn if arn]
+  sqs_queue_arns = [for arn in [var.drivewealth_sqs_arn, var.aws_events_sqs_arn] : arn if arn != ""]
 }
 
 ##################################################################################
