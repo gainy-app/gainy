d090cfc952adb95a26c01fe40cad3042ed3d8cc7
Mikhail Astashkevich <asmisha@tut.by>
2023-01-02 10:01:15 +0100

    fix skeleton

--- src/meltano/meltano/transform/models/portfolio/portfolio_chart_skeleton.sql
+++ src/meltano/meltano/transform/models/portfolio/portfolio_chart_skeleton.sql
@@ -12,34 +12,55 @@
 
 
 -- Execution Time: 143330.918 ms
+{% if is_incremental() %}
+with old_stats as materialized
+     (
+         select distinct on (
+             profile_id,
+             period
+             ) profile_id,
+               period,
+               datetime as max_datetime,
+               holding_count
+         from {{ this }}
+         order by profile_id desc, period desc, datetime desc
+     )
+{% endif %}
+
 select distinct on (
     profile_id, period, datetime
     ) profile_id,
       period,
       datetime,
+      t.holding_count,
       profile_id || '_' || period || '_' || datetime as id
 from (
          select t.*,
-                max(cnt)
-                over (partition by profile_id, period order by datetime rows between unbounded preceding and current row ) as cum_max_cnt
+                max(holding_count)
+                over (partition by profile_id, period order by datetime rows between unbounded preceding and current row ) as cum_max_holding_count
          from (
                   select profile_id,
                          period,
-                         date,
                          datetime,
-                         count(distinct holding_id_v2) as cnt
+                         count(distinct holding_id_v2) as holding_count
                   from {{ ref('portfolio_holding_chart') }}
 
-{% if is_incremental() and var('realtime') %}
-                           left join (
-                                        select profile_id, period, max(datetime) as max_datetime
-                                        from {{ this }}
-                                        group by profile_id, period
-                                     ) old_stats using (profile_id, period)
+{% if is_incremental() %}
+                           left join old_stats using (profile_id, period)
                   where old_stats.max_datetime is null or datetime > max_datetime
 {% endif %}
 
                   group by profile_id, period, date, datetime
+
+{% if is_incremental() %}
+                  union all
+
+                  select profile_id,
+                         period,
+                         max_datetime  as datetime,
+                         holding_count
+                  from old_stats
+{% endif %}
               ) t
      ) t
 
@@ -47,7 +68,7 @@ from (
          left join {{ this }} old_data using (profile_id, period, datetime)
 {% endif %}
 
-where cnt = cum_max_cnt
+where t.holding_count = cum_max_holding_count
 
 {% if is_incremental() %}
   and old_data.profile_id is null
