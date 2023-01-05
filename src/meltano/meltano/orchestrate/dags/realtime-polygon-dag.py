b3c57173ed7751e857b4f1ddaadae4911cd93159
Mikhail Astashkevich <asmisha@tut.by>
2023-01-05 18:04:54 +0100

    bump base image version and add generating config to realtime polygon dag

--- src/meltano/meltano/orchestrate/dags/realtime-polygon-dag.py
+++ src/meltano/meltano/orchestrate/dags/realtime-polygon-dag.py
@@ -1,5 +1,5 @@
 from airflow.operators.bash import BashOperator
-from common import create_dag, get_meltano_command, ENV, get_schedules
+from common import create_dag, get_meltano_command, ENV, get_schedules, MELTANO_PROJECT_ROOT
 
 dag_id = "realtime-polygon-dag"
 tags = ["meltano"]
@@ -8,6 +8,12 @@ dag = create_dag(dag_id,
                  tags=tags,
                  schedule_interval=schedule_interval,
                  is_paused_upon_creation=False)
+
+generate_meltano_config = BashOperator(
+    task_id="generate_meltano_config",
+    bash_command=
+    f"cd {MELTANO_PROJECT_ROOT}; /venv/bin/python scripts/generate_meltano_config.py",
+    dag=dag)
 
 schedules = get_schedules(include_skipped=True)
 for schedule in schedules:
@@ -19,3 +25,5 @@ for schedule in schedules:
         bash_command=get_meltano_command(
             f"schedule run {schedule['name']} --transform=skip"),
         dag=dag)
+
+    generate_meltano_config >> operator
