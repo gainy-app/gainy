29f80ad4e2ebed531bd81198a7a291343f092cc0
Mikhail Astashkevich <asmisha@tut.by>
2023-01-28 11:46:01 +0100

    fix

--- src/meltano/meltano/transform/models/drivewealth/drivewealth_holdings.sql
+++ src/meltano/meltano/transform/models/drivewealth/drivewealth_holdings.sql
@@ -87,4 +87,4 @@ select profile_id,
        null                              as collection_uniq_id,
        null                              as collection_id
 from {{ ref('trading_profile_status') }}
-where buying_power > 0
+where buying_power + pending_orders_sum > 0
