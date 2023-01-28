1e692bff8b2c1fb08d55904852833b2a4fb84e99
Mikhail Astashkevich <asmisha@tut.by>
2023-01-28 12:08:11 +0100

    Fix realtime portfolio names

--- src/meltano/meltano/transform/models/portfolio/portfolio_holding_group_details.sql
+++ src/meltano/meltano/transform/models/portfolio/portfolio_holding_group_details.sql
@@ -1,41 +1,41 @@
 {{
   config(
-    materialized = "table",
-    unique_key = "holding_group_id",
-    tags = ["realtime"],
-    post_hook=[
-      pk('holding_group_id'),
-      'create unique index if not exists "profile_id__ticker_symbol" ON {{ this }} (profile_id, ticker_symbol)',
-      'create unique index if not exists "profile_id__collection_id" ON {{ this }} (profile_id, collection_id)',
-    ]
+    materialized = "view",
   )
 }}
 
-select profile_holdings_normalized_all.holding_group_id,
-       profile_holdings_normalized_all.profile_id,
-       null as ticker_symbol, -- deprecated
-       collection_id, -- deprecated
-       min(portfolio_holding_details.purchase_date)                as purchase_date,
-       min(portfolio_holding_group_gains.relative_gain_total)      as relative_gain_total, -- deprecated
-       min(portfolio_holding_group_gains.relative_gain_1d)         as relative_gain_1d, -- deprecated
-       min(portfolio_holding_group_gains.value_to_portfolio_value) as value_to_portfolio_value, -- deprecated
-       null                                                        as ticker_name, -- deprecated
-       min(collections.name)                                       as name,
-       null                                                        as market_capitalization,
-       null                                                        as next_earnings_date,
-       null                                                        as ltt_quantity_total,
-       now()                                                       as updated_at
-from {{ ref('profile_holdings_normalized_all') }}
-         join {{ ref('portfolio_holding_details') }} using (holding_id_v2)
+select t.holding_group_id,
+       t.profile_id,
+       null                                                   as ticker_symbol,            -- deprecated
+       t.collection_id,                                                                    -- deprecated
+       t.purchase_date,
+       portfolio_holding_group_gains.relative_gain_total      as relative_gain_total,      -- deprecated
+       portfolio_holding_group_gains.relative_gain_1d         as relative_gain_1d,         -- deprecated
+       portfolio_holding_group_gains.value_to_portfolio_value as value_to_portfolio_value, -- deprecated
+       null                                                   as ticker_name,              -- deprecated
+       collections.name                                       as name,
+       null                                                   as market_capitalization,
+       null                                                   as next_earnings_date,
+       null                                                   as ltt_quantity_total,
+       now()                                                  as updated_at
+from (
+         select holding_group_id,
+                profile_id,
+                collection_id,
+                min(portfolio_holding_details.purchase_date) as purchase_date
+         from {{ ref('profile_holdings_normalized_dynamic') }}
+                  join {{ ref('portfolio_holding_details') }} using (holding_id_v2)
+         where collection_id is not null
+           and not profile_holdings_normalized_dynamic.is_hidden
+         group by holding_group_id, profile_id, collection_id
+     ) t
          join {{ ref('portfolio_holding_group_gains') }} using (profile_id, collection_id)
-         join {{ ref('collections') }} on collections.id = collection_id
-where not profile_holdings_normalized_all.is_hidden
-group by profile_holdings_normalized_all.holding_group_id, profile_holdings_normalized_all.profile_id, collection_id
+         join {{ ref('collections') }} on collections.id = t.collection_id
 
 union all
 
-select profile_holdings_normalized_all.holding_group_id,
-       profile_holdings_normalized_all.profile_id,
+select profile_holdings_normalized_dynamic.holding_group_id,
+       profile_holdings_normalized_dynamic.profile_id,
        portfolio_securities_normalized.ticker_symbol,
        null::int as collection_id,
        min(portfolio_holding_details.purchase_date)                as purchase_date,
@@ -51,13 +51,13 @@ select profile_holdings_normalized_all.holding_group_id,
        sum(portfolio_holding_details.ltt_quantity_total)           as ltt_quantity_total,
        now()                                                       as updated_at
 from {{ ref('portfolio_holding_details') }}
-         join {{ ref('profile_holdings_normalized_all') }} using (holding_id_v2)
+         join {{ ref('profile_holdings_normalized_dynamic') }} using (holding_id_v2)
          join {{ ref('portfolio_securities_normalized') }}
-              on portfolio_securities_normalized.id = profile_holdings_normalized_all.security_id
+              on portfolio_securities_normalized.id = profile_holdings_normalized_dynamic.security_id
          join {{ ref('portfolio_holding_group_gains') }}
-              on portfolio_holding_group_gains.profile_id = profile_holdings_normalized_all.profile_id
+              on portfolio_holding_group_gains.profile_id = profile_holdings_normalized_dynamic.profile_id
                   and portfolio_holding_group_gains.ticker_symbol = portfolio_securities_normalized.ticker_symbol
         left join {{ ref('base_tickers') }}
               on base_tickers.symbol = portfolio_securities_normalized.ticker_symbol
-where not profile_holdings_normalized_all.is_hidden
-group by profile_holdings_normalized_all.holding_group_id, profile_holdings_normalized_all.profile_id, portfolio_securities_normalized.ticker_symbol
+where not profile_holdings_normalized_dynamic.is_hidden
+group by profile_holdings_normalized_dynamic.holding_group_id, profile_holdings_normalized_dynamic.profile_id, portfolio_securities_normalized.ticker_symbol
