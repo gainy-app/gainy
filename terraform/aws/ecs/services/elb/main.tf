aa7ef9793d07911399f6e9264c9d7305cc84b532
Mikhail Astashkevich <asmisha@tut.by>
2023-02-15 14:57:51 +0100

    fix policy

--- terraform/aws/ecs/services/elb/main.tf
+++ terraform/aws/ecs/services/elb/main.tf
@@ -15,49 +15,21 @@ resource "aws_s3_bucket" "lb_logs" {
 
 resource "aws_s3_bucket_policy" "lb-bucket-policy" {
   bucket = aws_s3_bucket.lb_logs.id
-
+  # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html#attach-bucket-policy
   policy = <<POLICY
 {
-    "Id": "Policy",
-    "Version": "2012-10-17",
-    "Statement": [{
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": [
-                    "${data.aws_elb_service_account.main.arn}"
-                ]
-            },
-            "Action": [
-                "s3:PutObject"
-            ],
-            "Resource": "${aws_s3_bucket.lb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-        },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "delivery.logs.amazonaws.com"
-            },
-            "Action": [
-                "s3:PutObject"
-            ],
-            "Resource": "${aws_s3_bucket.lb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "delivery.logs.amazonaws.com"
-            },
-            "Action": [
-                "s3:GetBucketAcl"
-            ],
-            "Resource": "${aws_s3_bucket.lb_logs.arn}"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::127311923021:root"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::bucket-name/prefix/AWSLogs/your-aws-account-id/*"
+      "Resource": "${aws_s3_bucket.lb_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    }
+  ]
 }
 POLICY
 }
