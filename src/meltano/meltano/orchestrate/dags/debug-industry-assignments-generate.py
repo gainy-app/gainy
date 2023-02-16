9878916bad19e3a2de004fe474f94220b36aa191
Mikhail Astashkevich <asmisha@tut.by>
2023-02-16 15:00:05 +0100

    Profile attributes in Amplitude

--- src/meltano/meltano/orchestrate/dags/debug-industry-assignments-generate.py
+++ src/meltano/meltano/orchestrate/dags/debug-industry-assignments-generate.py
@@ -1,12 +1,11 @@
 from airflow.operators.bash import BashOperator
 from common import create_dag
 
-dag_id = "debug-industry-assignments-generator"
-tags = ["gainy-compute", "mlflow", "inference", "debug"]
+dag_id = "debug-sync-profiles-analytics-attributes"
+tags = ["gainy-compute", "debug"]
 dag = create_dag(dag_id, tags=tags)
 
 industry_assignments_generator = BashOperator(
-    task_id="industry-assignments-generator",
-    bash_command="gainy_industry_assignment predict",
-    skip_exit_code=-9,
+    task_id="sync-profiles-analytics-attributes",
+    bash_command="gainy_sync_profiles_analytics_attributes",
     dag=dag)
