ee1515cef67f885e6bd7a0c609564f3905fded2f
Michael Astashkevich <asmisha@users.noreply.github.com>
2023-03-07 15:37:09 +0100

    Realtime latest trading session (#815)

--- src/meltano/meltano/transform/models/collection/collection_latest_trading_session.sql
+++ src/meltano/meltano/transform/models/collection/collection_latest_trading_session.sql
@@ -4,8 +4,8 @@
   )
 }}
 
-select collection_uniq_id, max(week_trading_sessions_static.date) as date, max(open_at)::timestamp as open_at
+select collection_uniq_id, max(week_trading_sessions.date) as date, max(open_at)::timestamp as open_at
 from {{ ref('collection_ticker_actual_weights') }}
-         join {{ ref('week_trading_sessions_static') }} using (symbol)
+         join {{ ref('week_trading_sessions') }} using (symbol)
 where index = 0
 group by collection_uniq_id
