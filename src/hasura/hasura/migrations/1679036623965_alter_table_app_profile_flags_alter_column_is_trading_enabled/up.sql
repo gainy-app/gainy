08a7f05bacad72fc1658267dc9645d875064aa67
Mikhail Astashkevich <asmisha@tut.by>
2023-03-17 21:27:54 +0100

    fix

--- src/hasura/hasura/migrations/1679036623965_alter_table_app_profile_flags_alter_column_is_trading_enabled/up.sql
+++ src/hasura/hasura/migrations/1679036623965_alter_table_app_profile_flags_alter_column_is_trading_enabled/up.sql
@@ -2,4 +2,5 @@ alter table "app"."profile_flags" alter column "is_trading_enabled" set default 
 update app.profile_flags set is_trading_enabled = true where not is_trading_enabled;
 
 insert into app.profile_flags(profile_id)
-select id from app.profiles;
+select id from app.profiles
+on conflict do nothing;
