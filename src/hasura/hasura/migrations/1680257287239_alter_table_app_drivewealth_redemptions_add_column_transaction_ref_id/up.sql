07cedec273008e41b90bae6e121abed2ea776831
Mikhail Astashkevich <asmisha@tut.by>
2023-03-31 13:04:29 +0200

    fix

--- src/hasura/hasura/migrations/1680257287239_alter_table_app_drivewealth_redemptions_add_column_transaction_ref_id/up.sql
+++ src/hasura/hasura/migrations/1680257287239_alter_table_app_drivewealth_redemptions_add_column_transaction_ref_id/up.sql
@@ -2,5 +2,5 @@ alter table "app"."drivewealth_redemptions"
     add column "transaction_ref_id" varchar null;
 
 update "app"."drivewealth_redemptions"
-set transaction_ref_id = data ->> 'finTranRef'
+set transaction_ref_id = coalesce(data ->> 'finTranRef', data ->> 'finTranReference')
 where transaction_ref_id is null;
