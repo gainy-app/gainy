ee8da18b2fdc20d002279c737e892ddc5d4cc54a
Mikhail Astashkevich <asmisha@tut.by>
2023-04-08 08:38:21 +0200

    on_kyc_passed notifications

--- src/aws/lambda-python/lambda_python/tests/scripts/test_email_notifications.py
+++ src/aws/lambda-python/lambda_python/tests/scripts/test_email_notifications.py
@@ -1,19 +1,25 @@
 from gainy.context_container import ContextContainer
 
+profile_id = 1
+
 
 def run():
     with ContextContainer() as context_container:
         context_container.notification_service.on_kyc_status_approved(1)
         context_container.notification_service.on_kyc_status_rejected(1)
         context_container.notification_service.on_kyc_status_info_required(
-            1, 'some additional info')
+            profile_id, 'some additional info')
         context_container.notification_service.on_kyc_status_doc_required(1)
         context_container.notification_service.on_funding_account_linked(
-            1, '****1111', 'Plaid Saving')
+            profile_id, '****1111', 'Plaid Saving')
         context_container.notification_service.on_deposit_initiated(
-            1, 12345.67)
+            profile_id, 12345.67)
         context_container.notification_service.on_deposit_success(
-            1, 12345.67, '****1111')
+            profile_id, 12345.67, '****1111')
         context_container.notification_service.on_deposit_failed(
-            1, 12345.67, '****1111')
+            profile_id, 12345.67, '****1111')
         context_container.notification_service.on_withdraw_success(1, 12345.67)
+        context_container.notification_service.on_kyc_form_abandoned(
+            profile_id)
+        context_container.notification_service.on_kyc_passed1(profile_id)
+        context_container.notification_service.on_kyc_passed2(profile_id)
