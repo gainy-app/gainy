1cc1c56fed8bc2ff7d583036e0a8fa6fd352a67c
Michael Astashkevich <asmisha@users.noreply.github.com>
2023-04-19 08:00:44 +0200

    Fix account not open bug (#859)
    * Fix account not open bug
    
    * fix on_new_transaction as well

--- src/aws/lambda-python/lambda_python/trading/drivewealth/event_handlers/users_updated.py
+++ src/aws/lambda-python/lambda_python/trading/drivewealth/event_handlers/users_updated.py
@@ -10,8 +10,10 @@ class UsersUpdatedEventHandler(AbstractDriveWealthEventHandler):
         return event_type in ["users.updated", "users.created"]
 
     def handle(self, event_payload: dict):
+        # TODO: make thread safe for a user
         user_id = event_payload["userID"]
         user = self.provider.sync_user(user_id)
+        user = self.repo.refresh(user)
 
         # create or update account
         if user.profile_id:
