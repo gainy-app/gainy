182eac6ecdcda01b8e473bb46f813812955e4445
Mikhail Astashkevich <asmisha@tut.by>
2023-05-10 11:42:03 +0200

    Disable disconnecting bank account when it has pending deposits

--- src/aws/lambda-python/lambda_python/trading/exceptions.py
+++ src/aws/lambda-python/lambda_python/trading/exceptions.py
@@ -1,4 +1,18 @@
+from gainy.exceptions import BadRequestException
+
+
 class WrongTradingOrderStatusException(Exception):
 
     def __init__(self, message='Wrong TradingCollectionVersion Status', *args):
         super().__init__(message, *args)
+
+
+class CannotDeleteFundingAccountException(Exception):
+
+    def __init__(self, message=None, *args):
+        self.message = message
+        super().__init__(message, *args)
+
+
+class CannotDeleteFundingAccountHttpException(BadRequestException):
+    pass
