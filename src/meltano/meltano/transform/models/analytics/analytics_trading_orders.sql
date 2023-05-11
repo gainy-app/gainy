e27bdae200071aecf706c010560469db1467dcf7
Michael Astashkevich <asmisha@users.noreply.github.com>
2023-05-11 14:58:41 +0200

    Trading transactions analytics (#887)
    * Trading transactions analytics
    
    * add to pipeline
    
    * bump version

---
+++ src/meltano/meltano/transform/models/analytics/analytics_trading_orders.sql
@@ -0,0 +1,54 @@
+select profile_id,
+       profiles.created_at as sign_up_at,
+       email,
+       first_name,
+       last_name,
+       transaction_date,
+       transaction_amount,
+       transaction_status,
+       case
+           when transaction_source = 'AUTOMATIC' and transaction_amount is null
+               then 'rebalance'
+           when transaction_source = 'AUTOMATIC' and transaction_amount is not null
+               then 'auto'
+           when transaction_amount > 0
+               then 'buy'
+           when transaction_amount < 0
+               then 'sell'
+           end             as transaction_type,
+       product_type,
+       collection_id,
+       symbol,
+       uniq_id,
+       transactions.updated_at
+from (
+         select 'tcv_' || id        as uniq_id,
+                profile_id,
+                created_at          as transaction_date,
+                target_amount_delta as transaction_amount,
+                status              as transaction_status,
+                source              as transaction_source,
+                'ttf'               as product_type,
+                collection_id,
+                null::text          as symbol,
+                updated_at
+         from {{ source('app', 'trading_collection_versions')}}
+
+         union all
+
+         select 'to_' || id         as uniq_id,
+                profile_id,
+                created_at          as transaction_date,
+                target_amount_delta as transaction_amount,
+                status              as transaction_status,
+                source              as transaction_source,
+                'ttf'               as product_type,
+                null                as collection_id,
+                symbol,
+                updated_at
+         from {{ source('app', 'trading_orders')}}
+     ) transactions
+         left join (
+                       select id as profile_id, email, first_name, last_name, created_at
+                       from {{ source('app', 'profiles')}}
+                   ) profiles using (profile_id)
