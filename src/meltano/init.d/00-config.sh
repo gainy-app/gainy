91d5019c01e2198320ef759e95b3c748f5780bef
Mikhail Astashkevich <asmisha@tut.by>
2023-06-16 10:16:50 +0200

    patch migration

--- src/meltano/init.d/00-config.sh
+++ src/meltano/init.d/00-config.sh
@@ -1,2 +1,3 @@
 python scripts/generate_meltano_config.py
+patch -u /venv/lib64/python3.9/site-packages/meltano/migrations/versions/6828cc5b1a4f_create_dedicated_state_table.py -i scripts/patches/6828cc5b1a4f_create_dedicated_state_table.patch
 meltano upgrade database
