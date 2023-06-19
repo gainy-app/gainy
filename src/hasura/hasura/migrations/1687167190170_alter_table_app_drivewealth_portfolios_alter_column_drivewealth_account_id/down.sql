863b587df2b54e613de6026f9d9de9cab5e38bcf
Mikhail Astashkevich <asmisha@tut.by>
2023-06-19 11:35:48 +0200

    Fix no portfolio and duplicate portfolio issues

---
+++ src/hasura/hasura/migrations/1687167190170_alter_table_app_drivewealth_portfolios_alter_column_drivewealth_account_id/down.sql
@@ -0,0 +1,1 @@
+alter table "app"."drivewealth_portfolios" drop constraint "drivewealth_portfolios_drivewealth_account_id_key";
---
+++ src/hasura/hasura/migrations/1687167190170_alter_table_app_drivewealth_portfolios_alter_column_drivewealth_account_id/up.sql
@@ -0,0 +1,1 @@
+alter table "app"."drivewealth_portfolios" add constraint "drivewealth_portfolios_drivewealth_account_id_key" unique ("drivewealth_account_id");
